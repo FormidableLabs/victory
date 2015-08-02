ee40397f9e92d6bb7eaf3736a6f68ada03585084
Ross Donaldson <gastove@gmail.com>
2015-08-01 19:47:28 -0700

    Moar demo! MOAR!

--- demo/app.jsx
+++ demo/app.jsx
@@ -6,8 +6,13 @@ import _ from "lodash";
 const twoLinesData = [
   _.range(0, 100, 1).map((x) => {return {x, y: Math.sin(x)}}),
   _.range(0, 100, 1).map((x) => {return {x, y: Math.sin(x + 5)}}),
-  _.range(0, 100, 1).map((x) => {return {x, y: Math.sin(x + 10)}})
 ];
+
+const namedData = {
+  lineOne: _.range(0, 100, 1).map((x) => {return {x, y: Math.sin(x)}}),
+  lineTwo: _.range(0, 100, 1).map((x) => {return {x, y: Math.sin(x + 5)}}),
+  lineThree: _.range(0, 100, 1).map((x) => {return {x, y: Math.sin(x + 10)}})
+};
 
 class App extends React.Component {
 
@@ -18,10 +23,19 @@ class App extends React.Component {
           <VictoryChart />
           <VictoryChart y={(x) => Math.sin(x)}/>
           <VictoryChart y={[(x) => Math.sin(x),
-                            (x) => Math.sin(x + 5)
+                            (x) => Math.sin(x + 5),
                             (x) => Math.sin(x + 10)]}
-                        sample={25} />
-          <VictoryChart data={twoLinesData}/>
+                        sample={25}
+                        lineStyles={[{"stroke": "blue"},
+                                     {"stroke": "red"},
+                                     {"stroke": "orange"}]} />
+          <VictoryChart data={twoLinesData}
+                        lineStyles={[{"stroke": "green"},
+                                     {"stroke": "blue"}]} />
+          <VictoryChart data={namedData}
+                        lineStyles={{lineOne: {"stroke": "orange"},
+                                     lineThree: {stroke: "lightblue"},
+                                     lineTwo: {"stroke": "red"}}} />
         </p>
       </div>
     );
