1d949a989fbb4cab9b1a096ccde38d60a50156bc
Angela Nicholas <anicholas@walmartlabs.com>
2015-08-03 16:55:12 -0700

    angle and sorting props

--- src/components/victory-donut.jsx
+++ src/components/victory-donut.jsx
@@ -9,9 +9,39 @@ class VictoryDonut extends React.Component {
   constructor(props) {
     super(props);
     const radius = Math.min(this.props.width, this.props.height) / 2;
-    this.arc = d3.svg.arc().outerRadius(radius).innerRadius(radius - this.props.arcWidth);
+    const sortOrder = this.getSortOrder();
+
     this.colors = d3.scale.ordinal().range(this.props.arcColors);
-    this.pie = d3.layout.pie().sort(null).value((d) => { return d.y; });
+
+    this.arc = d3.svg.arc()
+      .outerRadius(radius)
+      .innerRadius(radius - this.props.arcWidth);
+
+    this.pie = d3.layout.pie()
+      .sort(sortOrder)
+      .startAngle(this.convertToRadians(this.props.startAngle))
+      .endAngle(this.convertToRadians(this.props.endAngle))
+      .padAngle(this.convertToRadians(this.props.padAngle))
+      .value((d) => { return d.y; });
+  }
+
+  convertToRadians(degrees) {
+    return degrees * (Math.PI / 180);
+  }
+
+  getSortOrder() {
+    let comparator = this.props.sort;
+
+    if (!_.isNull(comparator) && _.isString(comparator)) {
+      if (comparator === "ascending" || comparator === "descending") {
+        comparator = (a, b) => { return d3[this.props.sort](a.y, b.y); };
+      } else {
+        // console.error("Invalid sort order string. Try 'ascending' or 'descending'.");
+        comparator = null;
+      }
+    }
+
+    return comparator;
   }
 
   getStyles(fill) {
@@ -25,8 +55,8 @@ class VictoryDonut extends React.Component {
       },
       path: {
         fill,
-        stroke: this.props.edgeColor,
-        strokeWidth: this.props.edgeWidth
+        stroke: this.props.borderColor,
+        strokeWidth: this.props.borderWidth
       }
     };
   }
@@ -76,16 +106,26 @@ class VictoryDonut extends React.Component {
 }
 
 VictoryDonut.propTypes = {
-  arcColors: React.PropTypes.array,
+  arcColors: React.PropTypes.arrayOf(React.PropTypes.string),
   arcWidth: React.PropTypes.oneOfType([React.PropTypes.string, React.PropTypes.number]),
-  data: React.PropTypes.array,
-  edgeColor: React.PropTypes.string,
-  edgeWidth: React.PropTypes.oneOfType([React.PropTypes.string, React.PropTypes.number]),
+  data: React.PropTypes.arrayOf(React.PropTypes.shape({
+    x: React.PropTypes.oneOfType([React.PropTypes.string, React.PropTypes.number]),
+    y: React.PropTypes.number
+  })),
+  borderColor: React.PropTypes.string,
+  borderWidth: React.PropTypes.oneOfType([React.PropTypes.string, React.PropTypes.number]),
+  endAngle: React.PropTypes.number,
   fontColor: React.PropTypes.string,
   fontFamily: React.PropTypes.string,
   fontSize: React.PropTypes.oneOfType([React.PropTypes.string, React.PropTypes.number]),
   fontWeight: React.PropTypes.oneOfType([React.PropTypes.string, React.PropTypes.number]),
   height: React.PropTypes.number,
+  padAngle: React.PropTypes.number,
+  sort: React.PropTypes.oneOfType([
+    React.PropTypes.oneOf(["ascending", "descending"]),
+    React.PropTypes.func
+  ]),
+  startAngle: React.PropTypes.number,
   width: React.PropTypes.number
 };
 
@@ -107,13 +147,17 @@ VictoryDonut.defaultProps = {
     { x: "D", y: 1 },
     { x: "E", y: 2 }
   ],
-  edgeColor: "white",
-  edgeWidth: 1,
+  borderColor: "white",
+  borderWidth: 1,
+  endAngle: 360,
   fontColor: "black",
   fontFamily: "'Helvetica Neue', Helvetica, Arial, sans-serif",
   fontSize: 10,
   fontWeight: 400,
   height: 400,
+  padAngle: 0,
+  sort: null,
+  startAngle: 0,
   width: 400
 };
 
