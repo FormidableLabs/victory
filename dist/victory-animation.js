814080bc48f53bc53bdbe0d633e2ed1c1a10e9e6
Ken Wheeler <ken_wheeler@me.com>
2015-08-06 09:01:47 -0400

    Build

--- dist/victory-animation.js
+++ dist/victory-animation.js
@@ -64,6 +64,8 @@ return /******/ (function(modules) { // webpackBootstrap
 /* 1 */
 /***/ function(module, exports, __webpack_require__) {
 
+	/*global requestAnimationFrame, cancelAnimationFrame*/
+	
 	"use strict";
 	
 	Object.defineProperty(exports, "__esModule", {
@@ -95,32 +97,96 @@ return /******/ (function(modules) { // webpackBootstrap
 	    _classCallCheck(this, VictoryAnimation);
 	
 	    _get(Object.getPrototypeOf(VictoryAnimation.prototype), "constructor", this).call(this, props);
-	    this.state = this.props.data;
+	    /* defaults */
+	    this.state = Array.isArray(this.props.data) ? this.props.data[0] : this.props.data;
 	    this.interpolator = null;
 	    this.step = 0;
+	    this.queue = [];
+	    /* build easing function */
 	    this.ease = (0, _d3Ease.ease)(this.props.easing);
-	    this.startRaf = this.startRaf.bind(this);
+	    /*
+	      unlike React.createClass({}), there is no autobinding of this in ES6 classes
+	      so we bind functionToBeRunEachFrame to current instance of victory animation class
+	    */
+	    this.functionToBeRunEachFrame = this.functionToBeRunEachFrame.bind(this);
 	  }
+	
+	  /* lifecycle */
 	
 	  _createClass(VictoryAnimation, [{
 	    key: "componentWillReceiveProps",
 	    value: function componentWillReceiveProps(nextProps) {
-	      this.raf && cancelAnimationFrame(this.raf);
-	      this.interpolator = (0, _d3Interpolate.interpolate)(this.state, nextProps.data);
-	      this.step = 0;
-	      this.startRaf();
+	      var _this = this;
+	
+	      /* cancel existing loop if it exists */
+	      if (this.raf) {
+	        cancelAnimationFrame(this.raf);
+	      }
+	      /* If an object was supplied */
+	      if (Array.isArray(nextProps.data) === false) {
+	        /* compare cached version to next props */
+	        this.interpolator = (0, _d3Interpolate.interpolate)(this.state, nextProps.data);
+	        /* reset step to zero */
+	        this.step = 0;
+	        /* start request animation frame */
+	        this.raf = this.functionToBeRunEachFrame();
+	        /* If an array was supplied */
+	      } else {
+	          /* Build our tween queue */
+	          nextProps.data.forEach(function (data) {
+	            _this.queue.push(data);
+	          });
+	          /* Start traversing the tween queue */
+	          this.traverseQueue();
+	        }
 	    }
+	
+	    /* Traverse the tween queue */
 	  }, {
-	    key: "startRaf",
-	    value: function startRaf() {
+	    key: "traverseQueue",
+	    value: function traverseQueue() {
+	      if (this.queue.length > 0) {
+	        /* Get the next index */
+	        var data = this.queue[0];
+	        /* compare cached version to next props */
+	        this.interpolator = (0, _d3Interpolate.interpolate)(this.state, data);
+	        /* reset step to zero */
+	        this.step = 0;
+	        this.raf = this.functionToBeRunEachFrame();
+	      }
+	    }
+	
+	    /* every frame we... */
+	  }, {
+	    key: "functionToBeRunEachFrame",
+	    value: function functionToBeRunEachFrame() {
+	      /*
+	        step can generate imprecise values, sometimes greater than 1
+	        if this happens set the state to 1 and return, cancelling the loop
+	      */
 	      if (this.step >= 1) {
 	        this.step = 1;
 	        this.setState(this.interpolator(this.step));
+	        if (this.queue.length > 0) {
+	          cancelAnimationFrame(this.raf);
+	          this.queue.shift();
+	          this.traverseQueue();
+	        }
 	        return;
 	      }
+	      /*
+	        if we're not at the end of the loop, set the state by passing
+	        current step value that's transformed by the ease function to the
+	        interpolator, which is cached for performance whenever props are received
+	      */
 	      this.setState(this.interpolator(this.ease(this.step)));
+	      /* increase step by velocity */
 	      this.step += this.props.velocity;
-	      this.raf = requestAnimationFrame(this.startRaf);
+	      /*
+	        requestAnimationFrame calls a function on a frame.
+	        continue the loop by feeding functionToBeRunEachFrame
+	      */
+	      this.raf = requestAnimationFrame(this.functionToBeRunEachFrame);
 	    }
 	  }, {
 	    key: "render",
@@ -135,12 +201,15 @@ return /******/ (function(modules) { // webpackBootstrap
 	VictoryAnimation.propTypes = {
 	  velocity: _react2["default"].PropTypes.number,
 	  easing: _react2["default"].PropTypes.string,
-	  data: _react2["default"].PropTypes.object
+	  data: _react2["default"].PropTypes.oneOfType([_react2["default"].PropTypes.object, _react2["default"].PropTypes.array])
 	};
 	
 	VictoryAnimation.defaultProps = {
+	  /* velocity modifies step each frame */
 	  velocity: 0.02,
+	  /* easing modifies step each frame */
 	  easing: "poly-in-out",
+	  /* we got nothin' */
 	  data: {}
 	};
 	
