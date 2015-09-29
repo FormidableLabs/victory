335960e203c527c174881632849fe492486b2051
Paula Lavalle <lavalle.paula@gmail.com>
2015-09-29 16:27:17 -0700

    Run gulp pages. Remove comments from index.js

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-// TODO: Where is ./lib ??
 var Playground = require('./lib/playground');
 
 module.exports = Playground;
-
-// Victory's meta.js:
-// "use strict";
-//
-// // Meta configuration for webpack, etc.
-// module.exports = {
-//   FILE_NAME: "victory",
-//   LIB_NAME: "Victory"
-// };
