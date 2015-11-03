c50d1859c68c443927a588db9bfbb557103d67b3
kstrack-grose <kiristrackgrose@gmail.com>
2015-11-03 15:49:37 -0800

    clean code to pass linting

--- src/style.js
+++ src/style.js
@@ -34,13 +34,13 @@ export const calc = function (expr, precision) {
  * @param {String} name The name of the color scale to return (optional).
  * @returns {Array} An array of 5 hex string values composing a color scale.
  */
-export const getColorScale = function(name) {
+export const getColorScale = function (name) {
   const scales = {
     victory: ["#9f9f9f", "#e0dfe0", "#7e7e7e", "#d3d2d3", "#000000"],
     gray: ["#969696", "#f1f1f1", "#636363", "#cccccc", "#252525"],
     bluePurple: ["#8c96c6", "#edf8fb", "#8856a7", "#b3cde3", "#810f7c"],
     red: ["#de2d26", "#fee5d9", "#fb6a4a", "#fcae91", "#a50f15"],
     yellowBlue: ["#41b6c4", "#ffffcc", "#2c7fb8", "#a1dab4", "#253494"]
-  }
+  };
   return name ? scales[name] : scales.victory;
 };
