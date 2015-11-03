a0df207f31cce9062c2aa0de0939b1fe3345efa5
kstrack-grose <kiristrackgrose@gmail.com>
2015-11-03 15:06:13 -0800

    clean getColorScale and related comments

--- src/style.js
+++ src/style.js
@@ -28,32 +28,19 @@ export const calc = function (expr, precision) {
 };
 
 /**
- * Given a string description of what is to be colored by the function,
- * grayscaleColors will return a string hex value as chosen by the Victory
- * default styles.
- * @param {String} name A string saying what is to be colored.
- * @returns {String} The hex value representing the color.
+ * Given the name of a color scale, getColorScale will return an array
+ * of 5 hex string values in that color scale. If no 'name' parameter
+ * is given, it will return the Victory default grayscale.
+ * @param {String} name The name of the color scale to return (optional).
+ * @returns {Array} An array of 5 hex string values composing a color scale.
  */
-export const grayscaleColors = function(name) {
-  const grayscale = {
-    bar: "#9f9f9f",
-    axisLabel: "7e7e7e",
-    line: "#000000"
-  }
-  return name === 'axisLine' ? grayscale.bar : grayscale[name];
-};
-
-// this method is intended to generate a color scale
-// it can be called with a "name" parameter to request a specific scale
-// but if there is no param, it will return a random scale (eventually)
-// returns an array of 5 strings of hex values
-export const generateColorScale = function(name) {
+export const getColorScale = function(name) {
   const scales = {
-    formidable: ["#e0dfe0", "#d3d2d3", "#9f9f9f", "#7e7e7e", "#000000"],
-    grayscale: ["#f7f7f7", "#cccccc", "#9696996", "#636363", "#252525"],
-    bluePurple: ["#edf8fb", "#b3cde3", "#8c96c6", "#8856a7", "#810f7c"],
-    red: ["#fee5d9", "#fcae91", "#fb6a4a", "#de2d26", "#a50f15"],
-    yellowBlue: ["#ffffcc", "#a1dab4", "#41b6c4", "#2c7fb8", "#253494"]
+    victory: ["#9f9f9f", "#e0dfe0", "#7e7e7e", "#d3d2d3", "#000000"],
+    gray: ["#969696", "#f1f1f1", "#636363", "#cccccc", "#252525"],
+    bluePurple: ["#8c96c6", "#edf8fb", "#8856a7", "#b3cde3", "#810f7c"],
+    red: ["#de2d26", "#fee5d9", "#fb6a4a", "#fcae91", "#a50f15"],
+    yellowBlue: ["#41b6c4", "#ffffcc", "#2c7fb8", "#a1dab4", "#253494"]
   }
-  return scales[name];
+  return name ? scales[name] : scales.victory;
 };
