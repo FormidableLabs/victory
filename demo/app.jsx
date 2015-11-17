d2b061ed88681d201eedee0290187d98ef2f24c2
Brian Beck <exogen@gmail.com>
2015-11-17 15:32:28 -0800

    Deduplicate requestAnimationFrame scheduling code
    This unifies handling of array vs. object in the `data` prop. Code for
    setting up the animation was duplicated, and really they're doing nearly
    the same thing: if `data` is an array, extend the animation queue, if
    it's an object, then replace the animation queue with `data`.
    
    It also fixes a couple things:
    * If the initial `data` value is an array, then upon mount we'll start
      animating, starting at the first value, instead of ignoring everything
      but the first value.
    * Keep a handle to the `setTimeout` that controls our delay, because
      we'll need to cancel it when new data comes in or we unmount.
    
    /cc @kenwheeler @boygirl

--- demo/app.jsx
+++ demo/app.jsx
@@ -29,42 +29,62 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <button type="button" onClick={this.clickHandler}>Toggle X</button>
-        <VictoryAnimation data={
-          {
-            x: this.state.x,
-            w: this.state.w,
-            h: this.state.h,
-            color: this.state.color,
-            br: this.state.br,
-            rotate: this.state.rotate
-          }}
-        >
-          {(data) => {
-            return (
-              <div style={
-                {
-                  position: "relative",
-                  left: data.x,
-                  width: data.w,
-                  height: data.h,
-                  backgroundColor: data.color,
-                  color: "white",
-                  fontFamily: "Lucida Grande",
-                  padding: 40,
-                  borderRadius: data.br,
-                  textAlign: "center",
-                  alignItems: "center",
-                  display: "flex",
-                  fontSize: 40,
-                  transform: `rotate(${data.rotate}deg)`
-                }}
+        <div style={{ float: "left", width: 520, height: 520 }}>
+          <button type="button" onClick={this.clickHandler}>Toggle X</button>
+          <VictoryAnimation data={
+            {
+              x: this.state.x,
+              w: this.state.w,
+              h: this.state.h,
+              color: this.state.color,
+              br: this.state.br,
+              rotate: this.state.rotate
+            }}
+          >
+            {(data) => {
+              return (
+                <div style={
+                  {
+                    position: "relative",
+                    left: data.x,
+                    width: data.w,
+                    height: data.h,
+                    backgroundColor: data.color,
+                    color: "white",
+                    fontFamily: "Lucida Grande",
+                    padding: 40,
+                    borderRadius: data.br,
+                    textAlign: "center",
+                    alignItems: "center",
+                    display: "flex",
+                    fontSize: 40,
+                    transform: `rotate(${data.rotate}deg)`
+                  }}
+                >
+                  <div style={{textAlign: "center", width: "100%"}}>Test</div>
+                </div>
+              );
+            }}
+          </VictoryAnimation>
+        </div>
+        <div style={{ float: "left" }}>
+          <VictoryAnimation data={[
+            { background: "red" },
+            { background: "green" },
+            { background: "blue" } ]}
+            delay={500}
+          >
+            {(style) => (
+              <div style={{
+                  width: 100,
+                  height: 100,
+                  borderRadius: 50,
+                  ...style }}
               >
-                <div style={{textAlign: "center", width: "100%"}}>Test</div>
               </div>
-            );
-          }}
-        </VictoryAnimation>
+            )}
+          </VictoryAnimation>
+        </div>
       </div>
     );
   }
