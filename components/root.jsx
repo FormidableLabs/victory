9abbff04a8b9c09b5bc81b313012d4628ca0a837
David Davidson <daviddavidson.1988@gmail.com>
2015-11-20 16:13:06 -0800

    IndexRoute for +100 idiomatic

--- components/root.jsx
+++ components/root.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import { Routehandler } from 'react-router';
 
-import App from './app';
-
 class Root extends React.Component {
   render() {
     return (
       <div>
-        {this.props.children || <App />}
+        {this.props.children}
       </div>
     );
   }
--- router.jsx
+++ router.jsx
@@ -1,14 +1,16 @@
 import createBrowserHistory from 'history/lib/createBrowserHistory';
 import React from 'react';
 import { render } from 'react-dom';
-import { Router, Route } from 'react-router';
+import { Router, Route, IndexRoute } from 'react-router';
 
-import Root from './components/root';
+import App from './components/app';
 import Docs from './components/docs';
+import Root from './components/root';
 
 const routes = (
   <Router history={createBrowserHistory()}>
     <Route path="/" component={Root}>
+      <IndexRoute component={App} />
       <Route path="docs" component={Docs} />
     </Route>
   </Router>
