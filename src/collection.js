d7dd23591af95e4a72e65fd82946c9cee9f6d47c
Lauren <lauren.eastridge@formidablelabs.com>
2015-11-28 12:33:54 -0800

    add methods from chart

--- src/collection.js
+++ src/collection.js
@@ -11,3 +11,13 @@ export const containsOnlyStrings = function (collection) {
     return _.isString(item);
   });
 };
+
+export const isArrayOfArrays = function (collection) {
+  return _.isArray(collection) && _.every(collection, (item) => {
+    return _.isArray(item);
+  });
+};
+
+export const removeUndefined = function (arr) {
+  return _.filter(arr, (el) => el !== undefined);
+};
