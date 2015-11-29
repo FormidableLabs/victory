1766bc856a3e4c1732fb52693891aaf259e40cc8
Per Nilsson <per.nilsson@formidablelabs.com>
2015-11-29 14:19:57 -0800

    SImplify utility function logic
    Use underscore function references directly, functionally

--- src/collection.js
+++ src/collection.js
@@ -1,9 +1,7 @@
 import _ from "lodash";
 
 export const containsStrings = function (collection) {
-  return _.some(collection, (item) => {
-    return _.isString(item);
-  });
+  return _.some(collection, _.isString);
 };
 
 export const containsOnlyStrings = function (collection) {
@@ -11,9 +9,7 @@ export const containsOnlyStrings = function (collection) {
 };
 
 export const isArrayOfArrays = function (collection) {
-  return _.isArray(collection) && _.every(collection, (item) => {
-    return _.isArray(item);
-  });
+  return _.isArray(collection) && _.every(collection, _.isArray);
 };
 
 export const removeUndefined = function (arr) {
