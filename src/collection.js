6a1ffedbc0564a5090ac51342c17ed4f0f2d913b
Per Nilsson <per.nilsson@formidablelabs.com>
2015-11-29 17:01:13 -0800

    Fix behavior of containsOnlyStrings, isArrayOfArrays
    Wrap collection spec in top level describe

--- src/collection.js
+++ src/collection.js
@@ -1,15 +1,19 @@
 import _ from "lodash";
 
+export const isNonEmptyArray = function (collection) {
+  return _.isArray(collection) && collection.length > 0;
+};
+
 export const containsStrings = function (collection) {
   return _.some(collection, _.isString);
 };
 
 export const containsOnlyStrings = function (collection) {
-  return _.isArray(collection) && _.every(collection, _.isString);
+  return isNonEmptyArray(collection) && _.every(collection, _.isString);
 };
 
 export const isArrayOfArrays = function (collection) {
-  return _.isArray(collection) && _.every(collection, _.isArray);
+  return isNonEmptyArray(collection) && _.every(collection, _.isArray);
 };
 
 export const removeUndefined = function (arr) {
