29a22e0f12bd78c6e424d11035ab2399ce102998
Paula Lavalle <lavalle.paula@gmail.com>
2015-12-02 16:50:17 -0800

    Remove Container class from docs.jsx which gets imported

--- docs/docs.jsx
+++ docs/docs.jsx
@@ -3,21 +3,19 @@ import ReactDOM from 'react-dom';
 import Ecology from 'ecology';
 import Radium, { Style } from 'radium';
 
-import theme from './theme';
+import { VictoryTheme } from 'formidable-landers';
 
 @Radium
 class Docs extends React.Component {
   render() {
     return (
-      <div className="Container">
-        <div className="Copy">
-          <Ecology
-            overview={require('!!raw!./ecology.md')}
-            source={require('json!./victory-pie.json')}
-            scope={{React, ReactDOM, VictoryPie: require('../src/components/victory-pie')}}
-            playgroundtheme='elegant' />
-          <Style rules={theme}/>
-        </div>
+      <div>
+        <Ecology
+          overview={require('!!raw!./ecology.md')}
+          source={require('json!./victory-pie.json')}
+          scope={{React, ReactDOM, VictoryPie: require('../src/components/victory-pie')}}
+          playgroundtheme='elegant' />
+        <Style rules={VictoryTheme}/>
       </div>
     )
   }
