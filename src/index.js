8b418e874386b2e81635fbd8265c73516054fbb2
Lauren <lauren.eastridge@formidablelabs.com>
2016-01-14 14:11:03 -0800

    add data helper to util

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import Chart from "./chart";
 import * as Collection from "./collection";
+import Data from "./data";
 import * as Log from "./log";
 import Scale from "./scale";
 import * as Style from "./style";
@@ -9,6 +10,7 @@ import * as PropTypes from "./prop-types";
 export default {
   Chart,
   Collection,
+  Data,
   Log,
   Scale,
   Style,
