6c5072fc61ee8b688c295d1a31542f6041307207
Dale Bustad <dale@divmain.com>
2016-02-23 15:30:13 -0800

    Add multi-argument memoization function.

---
+++ src/victory-util/perf.js
@@ -0,0 +1,23 @@
+/**
+ * Memoizes multi-argument functions.
+ *
+ * NOTE: If `fn` receives another function as its argument, memoization is
+ * not guaranteed to work.  The function will be strigified, but it will
+ * no longer be a closure.
+ *
+ * @param  {Function}  fn  The function to memoize
+ *
+ * @return {Function}      Memoized `fn`.
+ */
+export const memoize = function memoize (fn) {
+  const cache = {};
+  return function () {
+    const args = Array.prototype.slice.call(arguments);
+    const hash = args.map((arg) => {
+      return (typeof arg === "string" || typeof arg === "number") ? arg : JSON.stringify(arg);
+    }).join("~");
+    return hash in cache ?
+      cache[hash] :
+      cache[hash] = fn.apply(this, args);  // eslint-disable-line no-invalid-this
+  };
+};
