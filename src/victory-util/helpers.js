ae6ddcee0d2f2f3af3c837c10b4bbaafbaa97732
Lauren <lauren.eastridge@formidablelabs.com>
2016-03-10 18:27:18 -0800

    add event methods to helpers

--- src/victory-util/helpers.js
+++ src/victory-util/helpers.js
@@ -1,6 +1,10 @@
 import defaults from "lodash/defaults";
 import isFunction from "lodash/isFunction";
 import property from "lodash/property";
+import partial from "lodash/partial";
+import set from "lodash/set";
+import assign from "lodash/assign";
+
 
 export default {
   getPadding(props) {
@@ -127,5 +131,42 @@ export default {
     }
     // otherwise, assume it is an array index, property key or path (_.property handles all three)
     return property(key);
+  },
+
+  getPartialEvents(events, index, childProps) {
+    return events ?
+      Object.keys(events).reduce((memo, eventName) => {
+        /* eslint max-params: 0 */
+        memo[eventName] = partial(
+          events[eventName],
+          partial.placeholder, // evt will still be the first argument for event handlers
+          childProps, // event handlers will have access to data component props, including data
+          index, // used in setting a unique state property
+          eventName // used in setting a unique state property
+        );
+        return memo;
+      }, {}) :
+      {};
+  },
+
+  getEvents(events, namespace) {
+    const stateName = `${namespace}State`;
+    const onEvent = (evt, childProps, index, eventName) => {
+      if (this.props.events[namespace] && this.props.events[namespace][eventName]) {
+        this.setState({
+          [stateName]: assign(
+            {},
+            this.state[stateName],
+            set({}, index, this.props.events[namespace][eventName](evt, childProps, index))
+          )
+        });
+      }
+    };
+
+    return events ?
+      Object.keys(this.props.events[namespace]).reduce((memo, event) => {
+        memo[event] = onEvent;
+        return memo;
+      }, {}) : {};
   }
 };
