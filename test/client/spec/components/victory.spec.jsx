402a8c75b03a3392ae4695f130a80851ef38e867
Lauren <lauren.eastridge@formidablelabs.com>
2016-03-17 15:23:04 -0700

    actually export area

--- test/client/spec/components/victory.spec.jsx
+++ test/client/spec/components/victory.spec.jsx
@@ -8,6 +8,7 @@ describe("victory", () => {
   it("has expected properties", () => {
     expect(Victory.VictoryAnimation).not.to.equal(undefined);
     expect(Victory.VictoryAxis).not.to.equal(undefined);
+    expect(Victory.VictoryArea).not.to.equal(undefined);
     expect(Victory.VictoryBar).not.to.equal(undefined);
     expect(Victory.VictoryChart).not.to.equal(undefined);
     expect(Victory.VictoryLine).not.to.equal(undefined);
