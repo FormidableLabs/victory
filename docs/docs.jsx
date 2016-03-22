f62e7308121b6953a3467f741e7502ca49f58947
Per Nilsson <per.nilsson@formidablelabs.com>
2016-03-21 18:51:07 -0700

    Wrap docs in StyleRoot

--- docs/docs.jsx
+++ docs/docs.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Ecology from "ecology";
-import Radium, { Style } from "radium";
+import Radium, { Style, StyleRoot } from "radium";
 import * as docgen from "react-docgen";
 import { VictoryTheme } from "formidable-landers";
 import { VictoryPie } from "../src/index";
@@ -9,14 +9,14 @@ import { VictoryPie } from "../src/index";
 class Docs extends React.Component {
   render() {
     return (
-      <div>
+      <StyleRoot>
         <Ecology
           overview={require("!!raw!./ecology.md")}
           source={docgen.parse(require("!!raw!../src/components/victory-pie"))}
           scope={{React, ReactDOM, VictoryPie}}
           playgroundtheme="elegant" />
         <Style rules={VictoryTheme}/>
-      </div>
+      </StyleRoot>
     )
   }
 }
