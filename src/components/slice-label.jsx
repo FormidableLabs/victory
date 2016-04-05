38aac4cbc41b8cba45c604405101d6ba66744554
Doron Pagot <doronpagot@gmail.com>
2016-04-05 12:50:28 +0300

    use events prop in VictoryLabel to correctly attach to events

--- src/components/slice-label.jsx
+++ src/components/slice-label.jsx
@@ -48,7 +48,7 @@ export default class SliceLabel extends React.Component {
         datum={props.datum}
         style={style}
         text={label}
-        {...events}
+        events={events}
       />
     );
   }
--- test/client/spec/components/victory-pie.spec.jsx
+++ test/client/spec/components/victory-pie.spec.jsx
@@ -8,6 +8,7 @@ import React from "react";
 import { shallow, mount } from "enzyme";
 import VictoryPie from "src/components/victory-pie";
 import Slice from "src/components/slice";
+import SliceLabel from "src/components/slice-label";
 
 describe("components/victory-pie", () => {
   describe("default component rendering", () => {
@@ -64,6 +65,21 @@ describe("components/victory-pie", () => {
       const Slices = wrapper.find(Slice);
       Slices.forEach((node, index) => {
         const initialProps = Slices.at(index).props();
+        node.simulate("click");
+        expect(clickHandler.called).to.equal(true);
+        // the first argument is the standard evt object
+        expect(clickHandler.args[index][1]).to.eql(initialProps);
+        expect(clickHandler.args[index][2]).to.eql(index);
+      });
+    });
+    it("attaches an event to label", () => {
+      const clickHandler = sinon.spy();
+      const wrapper = mount(
+        <VictoryPie events={{labels: {onClick: clickHandler}}}/>
+      );
+      const SliceLabels = wrapper.find(SliceLabel);
+      SliceLabels.forEach((node, index) => {
+        const initialProps = SliceLabels.at(index).props();
         node.simulate("click");
         expect(clickHandler.called).to.equal(true);
         // the first argument is the standard evt object
