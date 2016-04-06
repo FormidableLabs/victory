bd6001301e4f440b15f5aa9ee5a1c8060d07f4f1
Dale Bustad <dale@divmain.com>
2016-04-05 18:18:49 -0700

    Remove short-circuit for components without own data - they need animation prop.

--- src/victory-util/transitions.js
+++ src/victory-util/transitions.js
@@ -260,10 +260,6 @@ export function getTransitionPropsFactory(children, parentState, parentAnimate, 
   const transitionDurations = getTransitionDurations(children, childrenTransitions, parentAnimate);
 
   return function getTransitionProps(childProps, childType, index) { // eslint-disable-line max-statements,max-len
-    if (!childProps.data) {
-      return {};
-    }
-
     let animate = assign({}, childProps.animate || parentAnimate);
 
     if (childType.defaultTransitions) {
