5e39af0cfe6a5d0d190747deca26cdca0519a432
Stefan Huynh <huynh.stefan@gmail.com>
2016-05-10 16:09:30 -0700

    Add comments about future directions

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,5 +1,9 @@
 import { configure } from "@kadira/storybook";
 
+// Consider generalizing this so that this can be a part of the archetype:
+// https://github.com/kadirahq/react-storybook/blob/master/docs/
+// configure_storybook.md#loading-modules
+
 function loadStories() {
   require("../stories/victory-pie");
 }
--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,3 +1,5 @@
+// This file will not be necessary after this issue:
+// https://github.com/FormidableLabs/victory/issues/259
 module.exports = {
   resolve: {
     extensions: ["", ".js", ".jsx"]
