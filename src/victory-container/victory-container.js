7affed8c7467bd867c6f5d6c25e0f17f06f2f392
ebrillhart <emma.brillhart@gmail.com>
2016-06-08 13:22:19 -0700

    last commit didn't save the victory container file

---
+++ src/victory-container/victory-container.js
@@ -0,0 +1,35 @@
+import React, { PropTypes } from "react";
+
+export default class VictoryContainer extends React.Component {
+	static propTypes = {
+		style: PropTypes.object,
+		height: PropTypes.number,
+		width: PropTypes.number,
+		children: React.PropTypes.oneOfType([
+     React.PropTypes.arrayOf(React.PropTypes.node),
+     React.PropTypes.node
+   	]),
+   	title: PropTypes.string,
+   	desc: PropTypes.string
+	}
+
+	static defaultProps = {
+		title: "Victory Chart",
+		desc: ""
+	}
+
+	render() {
+		return (
+			<svg
+        style={style.parent}
+        viewBox={`0 0 ${this.props.width} ${this.props.height}`}
+        role="img"
+        aria-labelledby="title desc"
+      >
+      	<title id="title">{this.props.title}</title>
+        <desc id="desc">{this.props.desc}</desc>
+      	{this.props.children}
+      </svg>
+			)
+	}
+}
