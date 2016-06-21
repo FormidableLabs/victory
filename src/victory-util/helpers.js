430a346f5f1bbea2afea8b8e23783ac961ea1956
ebrillhart <emma.brillhart@gmail.com>
2016-06-21 13:05:35 -0700

    adding the modifyProps helper method to core

--- src/victory-util/helpers.js
+++ src/victory-util/helpers.js
@@ -140,6 +140,22 @@ export default {
       {};
   },
 
+  modifyProps(props, fallbackProps) {
+    const themeCheck = props.theme && props.theme.props;
+    const heightFallbacks = props.height || fallbackProps.props.height;
+    const widthFallbacks = props.width || fallbackProps.props.width;
+
+    const width = themeCheck ?
+    props.width || props.theme.props.width || fallbackProps.props.width :
+    widthFallbacks;
+
+    const height = themeCheck ?
+    props.height || props.theme.props.height || fallbackProps.props.height :
+    heightFallbacks;
+
+    return Object.assign({}, props, { width, height });
+  },
+
   getEvents(events, namespace) {
     const onEvent = (evt, childProps, index, eventName) => {
       if (this.props.events[namespace] && this.props.events[namespace][eventName]) {
