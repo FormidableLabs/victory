41997df2d295e9f829b9ca70186497105d94a119
ebrillhart <emma.brillhart@gmail.com>
2016-06-21 13:57:11 -0700

    make modify props method more agnostic

--- src/victory-util/helpers.js
+++ src/victory-util/helpers.js
@@ -142,18 +142,9 @@ export default {
 
   modifyProps(props, fallbackProps) {
     const themeCheck = props.theme && props.theme.props;
-    const heightFallbacks = props.height || fallbackProps.props.height;
-    const widthFallbacks = props.width || fallbackProps.props.width;
 
-    const width = themeCheck ?
-    props.width || props.theme.props.width || fallbackProps.props.width :
-    widthFallbacks;
-
-    const height = themeCheck ?
-    props.height || props.theme.props.height || fallbackProps.props.height :
-    heightFallbacks;
-
-    return Object.assign({}, props, { width, height });
+    return themeCheck ? defaults({}, props, props.theme.props, fallbackProps.props)
+    : defaults({}, props, fallbackProps.props);
   },
 
   getEvents(events, namespace) {
