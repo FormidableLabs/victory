40148d3088eb6832b47eb1e0585acaac9e872ff2
ebrillhart <emma.brillhart@gmail.com>
2016-06-22 14:35:52 -0700

    adding a default data set to the demo

--- demo/components/victory-candlestick-demo.js
+++ demo/components/victory-candlestick-demo.js
@@ -25,6 +25,17 @@ const style = {
   parent: {border: "1px solid #ccc", margin: "2%", maxWidth: "40%"}
 };
 
+const data = [
+  {x: new Date(2016, 6, 1), open: 9, close: 30, high: 56, low: 7},
+  {x: new Date(2016, 6, 2), open: 80, close: 40, high: 120, low: 10},
+  {x: new Date(2016, 6, 3), open: 50, close: 80, high: 90, low: 20},
+  {x: new Date(2016, 6, 4), open: 70, close: 22, high: 70, low: 5},
+  {x: new Date(2016, 6, 5), open: 20, close: 35, high: 50, low: 10},
+  {x: new Date(2016, 6, 6), open: 35, close: 30, high: 40, low: 3},
+  {x: new Date(2016, 6, 7), open: 30, close: 90, high: 95, low: 30},
+  {x: new Date(2016, 6, 8), open: 80, close: 81, high: 83, low: 75}
+];
+
 
 export default class App extends React.Component {
   constructor(props) {
@@ -54,16 +65,7 @@ export default class App extends React.Component {
 
         <VictoryCandlestick
           style={{parent: style.parent}}
-          data={[
-            {x: 50, open: 9, close: 30, high: 56, low: 7, label: "first"},
-            {x: 100, open: 80, close: 40, high: 120, low: 10, fill: "pink"},
-            {x: 150, open: 50, close: 80, high: 90, low: 20},
-            {x: 200, open: 70, close: 22, high: 70, low: 5},
-            {x: 250, open: 20, close: 35, high: 50, low: 10},
-            {x: 300, open: 35, close: 30, high: 40, low: 3},
-            {x: 350, open: 30, close: 90, high: 95, low: 30},
-            {x: 400, open: 80, close: 81, high: 83, low: 75, label: "last"}
-          ]}
+          data={data}
           size={8}
           events={[{
             target: "labels",
@@ -104,16 +106,7 @@ export default class App extends React.Component {
           <VictoryCandlestick
             style={{parent: style.parent}}
             candleColors={{positive: "purple", negative: "blue"}}
-            data={[
-              {x: new Date(2016, 6, 1), open: 9, close: 30, high: 56, low: 7},
-              {x: new Date(2016, 6, 2), open: 80, close: 40, high: 120, low: 10},
-              {x: new Date(2016, 6, 3), open: 50, close: 80, high: 90, low: 20},
-              {x: new Date(2016, 6, 4), open: 70, close: 22, high: 70, low: 5},
-              {x: new Date(2016, 6, 5), open: 20, close: 35, high: 50, low: 10},
-              {x: new Date(2016, 6, 6), open: 35, close: 30, high: 40, low: 3},
-              {x: new Date(2016, 6, 7), open: 30, close: 90, high: 95, low: 30},
-              {x: new Date(2016, 6, 8), open: 80, close: 81, high: 83, low: 75}
-            ]}
+            data={data}
             size={8}
           />
         </VictoryChart>
