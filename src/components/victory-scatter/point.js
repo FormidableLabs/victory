437658128e90a2612c6a07d2657a78d702a364ae
howardchi <howardchi@fougood.com>
2016-07-01 20:20:42 +0800

    fix loading d3 1.0.0 modules

--- src/components/victory-scatter/point.js
+++ src/components/victory-scatter/point.js
@@ -1,7 +1,6 @@
 import React, { PropTypes } from "react";
 import pathHelpers from "./path-helpers";
 
-
 export default class Point extends React.Component {
   static propTypes = {
     index: React.PropTypes.number,
