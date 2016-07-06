d55b01ebf41cc510d4b2cc989843463f55b32725
chilijung <chilijung@gmail.com>
2016-07-06 10:19:29 +0800

    add demo

--- demo/components/victory-errorbar-demo.js
+++ demo/components/victory-errorbar-demo.js
@@ -1,7 +1,7 @@
 /*global window:false */
 import React from "react";
 import { merge, random, range } from "lodash";
-import { VictoryErrorBar, VictoryChart } from "../../src/index";
+import { VictoryErrorBar, VictoryScatter, VictoryChart } from "../../src/index";
 import ErrorBar from "../../src/components/victory-errorbar/errorbar";
 import { VictoryContainer, VictoryTheme } from "victory-core";
 
@@ -15,6 +15,14 @@ const getData = () => {
     };
   });
 };
+
+const basicData = [
+  {x: 1, y: 1, errorX: [1, 0.5], errorY: 1},
+  {x: 2, y: 2, errorX: [1, 3], errorY: 1},
+  {x: 3, y: 3, errorX: [1, 3], errorY: [2, 3]},
+  {x: 4, y: 2, errorX: [1, 0.5], errorY: 1},
+  {x: 5, y: 1, errorX: [1, 0.5], errorY: 2}
+];
 
 const style = {
   parent: {border: "1px solid #ccc", margin: "2%", maxWidth: "40%"}
@@ -35,7 +43,7 @@ export default class App extends React.Component {
       this.setState({
         data: getData()
       });
-    }, 2000);
+    }, 10000);
   }
 
   componentWillUnmount() {
@@ -46,6 +54,12 @@ export default class App extends React.Component {
     return (
       <div className="demo">
         <h1>VictoryErrorBar</h1>
+
+        <VictoryChart>
+          <VictoryErrorBar data={basicData}/>
+          <VictoryScatter data={basicData}/>
+        </VictoryChart>
+
         <VictoryErrorBar
           style={style}
           width={500}
