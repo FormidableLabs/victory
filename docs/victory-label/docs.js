b48a7e6b427d2a7b1f0957ce75cff58b9602e7cc
Kyle Cesmat <kyle.cesmat@formidable.com>
2016-07-07 09:21:32 -0700

    add ecologyPlaygroundLoading function

--- docs/victory-label/docs.js
+++ docs/victory-label/docs.js
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { merge } from "lodash";
 import Ecology from "ecology";
 import Radium, { Style } from "radium";
 import * as docgen from "react-docgen";
-import { VictoryTheme, appendLinkIcon } from "formidable-landers";
+import { VictoryTheme, appendLinkIcon, ecologyPlaygroundLoading } from "formidable-landers";
 import { VictoryLabel } from "../../src/index";
 
 class Docs extends React.Component {
@@ -15,7 +16,7 @@ class Docs extends React.Component {
           source={docgen.parse(require("!!raw!../../src/victory-label/victory-label"))}
           scope={{React, ReactDOM, VictoryLabel}}
           playgroundtheme="elegant"
-          customRenderers={appendLinkIcon}
+          customRenderers={merge(appendLinkIcon, ecologyPlaygroundLoading)}
         />
         <Style rules={VictoryTheme}/>
       </div>
