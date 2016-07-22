488dcdfe75972638d3de75596baa8fe6964717a1
ebrillhart <emma.brillhart@gmail.com>
2016-07-22 14:08:57 -0700

    commit changes to checkout

--- demo/components/victory-line-demo.js
+++ demo/components/victory-line-demo.js
@@ -1,7 +1,7 @@
 /*global window:false */
 import React from "react";
 import { merge, random, range } from "lodash";
-import {VictoryLine, VictoryChart} from "../../src/index";
+import {VictoryLine, VictoryChart, VictoryAxis} from "../../src/index";
 import LineSegment from "../../src/components/victory-line/line-segment";
 import Point from "../../src/components/victory-scatter/point";
 import { VictoryContainer, VictoryTheme } from "victory-core";
@@ -115,80 +115,140 @@ export default class App extends React.Component {
       <div className="demo">
         <h1>VictoryLine</h1>
 
-          <VictoryLine
-            style={{parent: parentStyle, data: this.state.style}}
-            data={this.state.transitionData}
-            animate={{duration: 800}}
-            containerComponent={
-              <VictoryContainer
-                title="Line Chart"
-                desc="This is a line chart for displaying data."
-              />
-            }
-          />
+          {/*<VictoryLine
+                      style={{parent: parentStyle, data: this.state.style}}
+                      data={this.state.transitionData}
+                      animate={{duration: 800}}
+                      containerComponent={
+                        <VictoryContainer
+                          title="Line Chart"
+                          desc="This is a line chart for displaying data."
+                        />
+                      }
+                    />
+          
+                  <VictoryLine
+                    style={{parent: parentStyle, data: this.state.style}}
+                    data={this.state.data}
+                    label={"label\none"}
+                    animate={{duration: 1500}}
+                  />
+          
+                  <VictoryLine
+                    style={{parent: parentStyle, data: {stroke: "blue"}}}
+                    y={(d) => Math.sin(2 * Math.PI * d.x)}
+                    sample={25}
+                  />
+          
+                  <VictoryLine
+                    style={{
+                      parent: parentStyle,
+                      data: {stroke: "red", strokeWidth: 6}
+                    }}
+                    events={[{
+                      target: "data",
+                      eventHandlers: {
+                        onClick: () => {
+                          return [
+                            {
+                              mutation: (props) => {
+                                return {style: merge({}, props.style, {stroke: "orange"})};
+                              }
+                            }, {
+                              target: "labels",
+                              mutation: () => {
+                                return {text: "hey"};
+                              }
+                            }
+                          ];
+                        }
+                      }
+                    }]}
+                    label={this.state.label}
+                    data={range(0, 100)}
+                    x={null}
+                    y={(d) => d * d}
+                  />
+          
+                  <VictoryLine
+                    style={{parent: parentStyle}}
+                    data={this.state.arrayData}
+                    x={0}
+                    y={1}
+                    dataComponent={<PointedLine />}
+                  />
+          
+                  <VictoryLine
+                    style={{parent: parentStyle}}
+                    data={this.state.arrayData}
+                    x={0}
+                    y={1}
+                  />
+          
+                  <VictoryLine
+                    style={{parent: parentStyle}}
+                    data={[
+                      {x: new Date(1982, 1, 1), y: 125},
+                      {x: new Date(1987, 1, 1), y: 257},
+                      {x: new Date(1993, 1, 1), y: 345},
+                      {x: new Date(1997, 1, 1), y: 515},
+                      {x: new Date(2001, 1, 1), y: 132},
+                      {x: new Date(2005, 1, 1), y: 305},
+                      {x: new Date(2011, 1, 1), y: 270},
+                      {x: new Date(2015, 1, 1), y: 470}
+                    ]}
+                  />
+          
+                  <VictoryLine
+                    style={{parent: parentStyle}}
+                    data={[
+                      {x: 1, y: 1},
+                      {x: 2, y: 3},
+                      {x: 3, y: 5},
+                      {x: 4, y: 2},
+                      {x: 5, y: null},
+                      {x: 6, y: null},
+                      {x: 7, y: 6},
+                      {x: 8, y: 7},
+                      {x: 9, y: 8},
+                      {x: 10, y: 12}
+                    ]}
+                  />
+          
+                  <VictoryLine
+                    style={{parent: parentStyle}}
+                    scale={{x: "linear", y: "log"}}
+                  />
+                  <VictoryLine
+                    style={{parent: parentStyle}}
+                    data={this.state.arrayData}
+                    label="Hello"
+                    x={0}
+                    y={1}
+                    theme={VictoryTheme.grayscale}
+                  />
+          
+                  <VictoryChart
+                    theme={VictoryTheme.grayscale}
+                  >
+                    <VictoryLine
+                      style={{parent: parentStyle}}
+                      data={this.state.arrayData}
+                      label="Hello"
+                      x={0}
+                      y={1}
+                    />
+                </VictoryChart>*/}
 
+      <VictoryChart
+        height={450}
+        scale={{
+          x: "time"
+        }}
+      >
         <VictoryLine
-          style={{parent: parentStyle, data: this.state.style}}
-          data={this.state.data}
-          label={"label\none"}
-          animate={{duration: 1500}}
-        />
-
-        <VictoryLine
-          style={{parent: parentStyle, data: {stroke: "blue"}}}
-          y={(d) => Math.sin(2 * Math.PI * d.x)}
-          sample={25}
-        />
-
-        <VictoryLine
-          style={{
-            parent: parentStyle,
-            data: {stroke: "red", strokeWidth: 6}
-          }}
-          events={[{
-            target: "data",
-            eventHandlers: {
-              onClick: () => {
-                return [
-                  {
-                    mutation: (props) => {
-                      return {style: merge({}, props.style, {stroke: "orange"})};
-                    }
-                  }, {
-                    target: "labels",
-                    mutation: () => {
-                      return {text: "hey"};
-                    }
-                  }
-                ];
-              }
-            }
-          }]}
-          label={this.state.label}
-          data={range(0, 100)}
-          x={null}
-          y={(d) => d * d}
-        />
-
-        <VictoryLine
-          style={{parent: parentStyle}}
-          data={this.state.arrayData}
-          x={0}
-          y={1}
-          dataComponent={<PointedLine />}
-        />
-
-        <VictoryLine
-          style={{parent: parentStyle}}
-          data={this.state.arrayData}
-          x={0}
-          y={1}
-        />
-
-        <VictoryLine
-          style={{parent: parentStyle}}
           data={[
-            {x: new Date(1982, 1, 1), y: 125},
+            {x: new Date(1960, 1, 1), y: 125},
             {x: new Date(1987, 1, 1), y: 257},
             {x: new Date(1993, 1, 1), y: 345},
             {x: new Date(1997, 1, 1), y: 515},
@@ -198,46 +258,6 @@ export default class App extends React.Component {
             {x: new Date(2015, 1, 1), y: 470}
           ]}
         />
-
-        <VictoryLine
-          style={{parent: parentStyle}}
-          data={[
-            {x: 1, y: 1},
-            {x: 2, y: 3},
-            {x: 3, y: 5},
-            {x: 4, y: 2},
-            {x: 5, y: null},
-            {x: 6, y: null},
-            {x: 7, y: 6},
-            {x: 8, y: 7},
-            {x: 9, y: 8},
-            {x: 10, y: 12}
-          ]}
-        />
-
-        <VictoryLine
-          style={{parent: parentStyle}}
-          scale={{x: "linear", y: "log"}}
-        />
-        <VictoryLine
-          style={{parent: parentStyle}}
-          data={this.state.arrayData}
-          label="Hello"
-          x={0}
-          y={1}
-          theme={VictoryTheme.grayscale}
-        />
-
-        <VictoryChart
-          theme={VictoryTheme.grayscale}
-        >
-          <VictoryLine
-            style={{parent: parentStyle}}
-            data={this.state.arrayData}
-            label="Hello"
-            x={0}
-            y={1}
-          />
       </VictoryChart>
       </div>
     );
