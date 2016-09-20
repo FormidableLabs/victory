16e389f28a9b6818827ddfa78cefc7b26b2f88c8
Lauren <lauren.eastridge@formidable.com>
2016-09-20 11:51:25 -0700

    fix specs

--- test/client/spec/victory-tooltip/victory-tooltip.spec.js
+++ test/client/spec/victory-tooltip/victory-tooltip.spec.js
@@ -9,7 +9,7 @@ import VictoryTooltip from "src/victory-tooltip/victory-tooltip";
 import Flyout from "src/victory-primitives/flyout";
 
 describe("components/victory-label", () => {
-  const baseProps = {x: 0, y: 0, active: true, text: "such text, wow"};
+  const baseProps = {x: 0, y: 0, active: true, text: "such text, wow", renderInPortal: false};
   it("renders nothing when not active", () => {
     const wrapper = mount(
       <VictoryTooltip {...baseProps} active={false}/>
