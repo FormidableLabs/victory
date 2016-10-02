a523b474afc825a88a213b62c20c4749d93d2907
Lauren <lauren.eastridge@formidable.com>
2016-10-02 14:28:49 -0700

    remove unnecessary helpers, annotate events

--- src/victory-util/helpers.js
+++ src/victory-util/helpers.js
@@ -127,50 +127,10 @@ export default {
     return property(key);
   },
 
-  getPartialEvents(events, index, childProps) {
-    return events ?
-      Object.keys(events).reduce((memo, eventName) => {
-        /* eslint max-params: 0 */
-        memo[eventName] = partial(
-          events[eventName],
-          partial.placeholder, // evt will still be the first argument for event handlers
-          childProps, // event handlers will have access to data component props, including data
-          index, // used in setting a unique state property
-          eventName // used in setting a unique state property
-        );
-        return memo;
-      }, {}) :
-      {};
-  },
-
   modifyProps(props, fallbackProps, role) {
     const theme = props.theme && props.theme[role] ? props.theme[role] : {};
     const themeProps = omit(theme, ["style"]);
     const baseProps = defaults({}, props, themeProps, fallbackProps);
     return defaults({}, baseProps, {clipWidth: baseProps.width, clipHeight: baseProps.height});
-  },
-
-  getEvents(events, namespace) {
-    const onEvent = (evt, childProps, index, eventName) => {
-      if (this.props.events[namespace] && this.props.events[namespace][eventName]) {
-        this.setState({
-          [index]: merge(
-            {},
-            this.state[index],
-            this.props.events[namespace][eventName](evt, childProps, index)
-          )
-        });
-      }
-    };
-
-    return events ?
-      Object.keys(this.props.events[namespace]).reduce((memo, event) => {
-        memo[event] = onEvent;
-        return memo;
-      }, {}) : {};
-  },
-
-  getEventState(index, namespace) {
-    return this.state[index] && this.state[index][namespace];
   }
 };
