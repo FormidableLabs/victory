63d189403358e4b1137eaf13416c634a16a12f82
Lauren <lauren.eastridge@formidable.com>
2016-10-17 16:19:46 +0100

    check for explicit false

--- src/victory-util/transitions.js
+++ src/victory-util/transitions.js
@@ -329,7 +329,7 @@ export function getTransitionPropsFactory(props, state, setState) {
   };
 
   const onExit = (nodes, child, data, animate) => { // eslint-disable-line max-params
-    if (!nodesDoneClipPathExit) {
+    if (nodesDoneClipPathExit === false) {
       return getChildClipPathToExit(animate, child, data, nodes, () => {
         setState({ nodesWillExit: false, nodesDoneClipPathExit: true });
       });
