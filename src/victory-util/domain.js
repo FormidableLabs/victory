c2cabf9e7d540cfe6e93964ba5bb933d994a4f48
Lauren <lauren.eastridge@formidable.com>
2016-10-27 11:23:47 -0700

    change how min = max domain charts are handled

--- src/victory-util/domain.js
+++ src/victory-util/domain.js
@@ -116,7 +116,7 @@ export default {
     // TODO: is this the correct behavior, or should we just error. How do we
     // handle charts with just one data point?
     if (min === max) {
-      const adjustedMax = max === 0 ? 1 : max + max;
+      const adjustedMax = max === 0 ? 1 : max;
       return [0, adjustedMax];
     }
     return [min, max];
