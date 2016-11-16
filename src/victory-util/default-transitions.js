45d7690efedc1500e5ad8c1bc3e1d94fe6c25feb
Lauren <lauren.eastridge@formidable.com>
2016-11-15 21:08:24 -0800

    remove default clipWidth transtions

--- src/victory-util/default-transitions.js
+++ src/victory-util/default-transitions.js
@@ -1,75 +1,14 @@
 /* eslint-disable func-style */
-import { filter, max, min, sum } from "lodash";
-import Helpers from "./helpers";
-import Log from "./log";
-
 export function continuousTransitions() {
   return {
     onLoad: {
-      duration: 2000,
-      entrance: "left",
-      beforeClipPathWidth: (data, child, animate) => {
-        // const range = Helpers.getRange(child.props, "x");
-        // const paddingLeft = range[0];
-        // const paddingRight = child.props.width - range[1]; // eslint-disable-line max-len
-        // if (animate.onLoad.entrance === "left") {
-        //   return {
-        //     clipWidth: paddingLeft + paddingRight
-        //   };
-        // } else if (animate.onLoad.entrance === "right") {
-        //   return {
-        //     clipWidth: paddingLeft + paddingRight,
-        //     translateX: child.props.width - paddingLeft - paddingRight
-        //   };
-        // } else {
-        //   Log.warn("onLoad entrance should be one of left or right");
-        //   return {};
-        // }
-      },
-      afterClipPathWidth: (data, child, animate) => {
-      //   const range = Helpers.getRange(child.props, "x");
-      //   if (animate.onLoad.entrance === "left") {
-      //     return {
-      //       clipWidth: sum(range)
-      //     };
-      //   } else if (animate.onLoad.entrance === "right") {
-      //     return {
-      //       clipWidth: sum(range),
-      //       translateX: 0
-      //     };
-      //   } else {
-      //     Log.warn("onLoad entrance should be one of left or right");
-      //     return {};
-      //   }
-      }
+      duration: 2000
     },
     onExit: {
-      duration: 500,
-      beforeClipPathWidth: (data, child, exitingNodes) => {
-        const xVals = data.map((datum) => {
-          return child.type.getScale(child.props).x(datum.x);
-        });
-        const clipPath = min(xVals) + max(xVals);
-        return clipPath;
-      }
+      duration: 500
     },
     onEnter: {
-      duration: 500,
-      beforeClipPathWidth: (data, child) => {
-        const xVals = data.map((datum) => {
-          return child.type.getScale(child.props).x(datum.x);
-        });
-        const clipPath = min(xVals) + max(xVals);
-        return clipPath;
-
-      },
-      afterClipPathWidth: (data, child) => {
-        const xVals = data.map((datum) => {
-          return child.type.getScale(child.props).x(datum.x);
-        });
-        const clipPath = min(xVals) + max(xVals);
-        return clipPath;
-      }
+      duration: 500
     }
   };
 }
