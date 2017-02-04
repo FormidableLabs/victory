e79568fdf7131ae54561a18d6056c0e9f4e28afa
boygirl <lauren.eastridge@formidable.com>
2017-02-03 21:09:57 -0800

    correct SelectionHelpers export

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ export { default as VictoryBrushContainer } from "./components/containers/victor
 export { default as VictoryZoomContainer } from "./components/containers/victory-zoom-container";
 export { default as VictoryZoom } from "./components/victory-zoom/victory-zoom";
 export { default as BrushHelpers } from "./components/containers/brush-helpers";
-export { default as Selectionelpers } from "./components/containers/selection-helpers";
+export { default as SelectionHelpers } from "./components/containers/selection-helpers";
 export { default as ZoomHelpers } from "./components/containers/zoom-helpers";
 
 
