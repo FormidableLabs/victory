82379d3051ec7e057ed175dce8fc02d558e18422
Derek Maffett <derekgmaffett@gmail.com>
2017-02-05 17:02:17 -0800

    use mocks for tests

--- test/client/spec/victory-shared-events/victory-shared-events.spec.js
+++ test/client/spec/victory-shared-events/victory-shared-events.spec.js
@@ -4,13 +4,14 @@
 import { memoize, forEach } from "lodash";
 import { mount } from "enzyme";
 import React from "react";
+import { addEvents } from "src/index";
 import VictorySharedEvents from "src/victory-shared-events/victory-shared-events";
-import { VictoryPie } from "victory-pie";
-import { VictoryBar, VictoryScatter } from "victory-chart";
-import { Slice, Bar, Point } from "src/victory-primitives";
+import { MockVictoryComponent, MockLabel, MockDataComponent } from "../mock-components";
 
 describe("components/victory-shared-events", () => {
-  it("should trigger shared events exclusively on selected children", () => {
+  const EventedMockVictoryComponent = addEvents(MockVictoryComponent);
+
+  it.only("should trigger shared events exclusively on selected children", () => {
     const data = [
       {x: "a", y: 2},
       {x: "b", y: 3},
@@ -21,12 +22,12 @@ describe("components/victory-shared-events", () => {
       <svg>
         <VictorySharedEvents
           events={[{
-            childName: ["pie", "bar"],
+            childName: ["one", "two"],
             target: "data",
             eventHandlers: {
               onClick: () => {
                 return [{
-                  childName: ["pie", "scatter"],
+                  childName: ["one", "three"],
                   mutation: (props) => {
                     return {
                       style: Object.assign({}, props.style, {fill: "tomato"})
@@ -37,34 +38,31 @@ describe("components/victory-shared-events", () => {
             }
           }]}
         >
-          <VictoryBar name="bar"
-            data={data}
-            dataComponent= {< Bar />}
-          />
-          <VictoryPie name="pie"
-            data={data}
-            dataComponent={< Slice />}
-          />
-          <VictoryScatter name="scatter"
-            data={data}
-            dataComponent={< Point />}
-          />
+          <EventedMockVictoryComponent name="one" data={data} />
+          <EventedMockVictoryComponent name="two" data={data} />
+          <EventedMockVictoryComponent name="three" data={data} />
         </VictorySharedEvents>
       </svg>
     );
 
-    const findDataComponent = memoize((type, index, component) => {
-      return component.find(type).filterWhere((dataComponent) => {
-        return dataComponent.props().index === index;
-      });
-    }, (type, index) => {
-      return type + index;
+    const findDataComponent = memoize((name, index, component) => {
+      return component
+        .find(EventedMockVictoryComponent)
+        .filterWhere((victoryComponent) => {
+          return victoryComponent.props().name === name;
+        })
+        .find(MockDataComponent)
+        .filterWhere((dataComponent) => {
+          return dataComponent.props().index === index;
+        });
+    }, (name, index) => {
+      return name + index;
     });
 
     const expectEventEffects = (expectationMatrix, component) => {
-      forEach(expectationMatrix, (dataComponents, dataComponentType) => {
+      forEach(expectationMatrix, (dataComponents, parentComponentName) => {
         forEach(dataComponents, (eventExpectation, index) => {
-          const node = findDataComponent(dataComponentType, index, component);
+          const node = findDataComponent(parentComponentName, index, component);
           const eventTriggeredOnComponent = node.props().style.fill === "tomato";
 
           expect(eventTriggeredOnComponent).to.eql(eventExpectation);
@@ -74,36 +72,36 @@ describe("components/victory-shared-events", () => {
 
     // Expect no events triggered at beginning.
     expectEventEffects({
-      Slice: [false, false, false, false],
-      Bar: [false, false, false, false],
-      Point: [false, false, false, false]
+      one: [false, false, false, false],
+      two: [false, false, false, false],
+      three: [false, false, false, false]
     }, wrapper);
 
-    findDataComponent(Slice, 0, wrapper).simulate("click");
+    findDataComponent('one', 0, wrapper).simulate("click");
 
     // Pie triggers effects on pie and scatter plot
     expectEventEffects({
-      Slice: [true, false, false, false],
-      Bar: [false, false, false, false],
-      Point: [true, false, false, false]
+      one: [true, false, false, false],
+      two: [false, false, false, false],
+      three: [true, false, false, false]
     }, wrapper);
 
-    findDataComponent(Bar, 1, wrapper).simulate("click");
+    findDataComponent('two', 1, wrapper).simulate("click");
 
     // Bar triggers effects on pie and scatter plot
     expectEventEffects({
-      Slice: [true, true, false, false],
-      Bar: [false, false, false, false],
-      Point: [true, true, false, false]
+      one: [true, true, false, false],
+      two: [false, false, false, false],
+      three: [true, true, false, false]
     }, wrapper);
 
-    findDataComponent(Point, 2, wrapper).simulate("click");
+    findDataComponent('three', 2, wrapper).simulate("click");
 
     // Scatter does not trigger effects
     expectEventEffects({
-      Slice: [true, true, false, false],
-      Bar: [false, false, false, false],
-      Point: [true, true, false, false]
+      one: [true, true, false, false],
+      two: [false, false, false, false],
+      three: [true, true, false, false]
     }, wrapper);
   });
 });
