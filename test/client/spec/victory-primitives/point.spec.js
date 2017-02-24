9fd2697f05eed35dfa6e9fc633adeb66a445e7a1
Derek Maffett <derekgmaffett@gmail.com>
2017-02-24 15:34:06 -0800

    test all symbols

--- test/client/spec/victory-primitives/point.spec.js
+++ test/client/spec/victory-primitives/point.spec.js
@@ -21,24 +21,24 @@ describe("victory-primitives/point", () => {
     sandbox.restore();
   });
 
-  it.only("should render the appropriate symbol", () => {
-    const circleStub = sandbox.stub(pathHelpers, "circle").returns("circle symbol");
-    const squareStub = sandbox.stub(pathHelpers, "square").returns("square symbol");
-    const circleProps = Object.assign({}, baseProps, {symbol: "circle"});
-    const squareProps = Object.assign({}, baseProps, {symbol: "square"});
+  it("should render the appropriate symbol", () => {
+    [
+      "circle",
+      "square",
+      "diamond",
+      "triangleDown",
+      "triangleUp",
+      "plus",
+      "star"
+    ].forEach((symbol) => {
+      const stub = sandbox.stub(pathHelpers, symbol).returns(symbol + " symbol");
+      const props = Object.assign({}, baseProps, {symbol});
+      const wrapper = shallow(<Point {...props}/>);
+      const directions = wrapper.render().find("path").attr("d");
 
-    const circleWrapper = shallow(<Point {...circleProps}/>);
-    const squareWrapper = shallow(<Point {...squareProps}/>);
-    const circleDirections = circleWrapper.render().find("path").attr("d");
-    const squareDirections = squareWrapper.render().find("path").attr("d");
-
-    expect(circleStub.callCount).to.eql(1);
-    expect(circleStub.getCall(0).args).to.eql([5, 10, 1]);
-    expect(circleDirections).to.eql("circle symbol");
-
-    expect(squareStub.callCount).to.eql(1);
-    expect(squareStub.getCall(0).args).to.eql([5, 10, 1]);
-    expect(squareDirections).to.eql("square symbol");
-
+      expect(stub.callCount).to.eql(1);
+      expect(stub.getCall(0).args).to.eql([5, 10, 1]);
+      expect(directions).to.eql(symbol + " symbol");
+    });
   });
 });
