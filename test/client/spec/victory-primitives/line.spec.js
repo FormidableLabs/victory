b468350f0c7269871b253a0e7d3d16f8cb2ab6ac
Derek Maffett <derekgmaffett@gmail.com>
2017-02-28 15:19:09 -0800

    fix eslint errors

--- test/client/spec/victory-primitives/line.spec.js
+++ test/client/spec/victory-primitives/line.spec.js
@@ -1,16 +1,13 @@
-
 import React from "react";
 import { shallow } from "enzyme";
 import Line from "src/victory-primitives/line";
-import SvgTestHelper from "../svg-test-helper";
-import { merge } from "lodash";
 
 describe("victory-primitives/line", () => {
   const baseProps = {
     x1: 0,
     y1: 1,
     x2: 2,
-    y2: 4,
+    y2: 4
   };
 
   it("should render a line element with the correct coordinates", () => {
