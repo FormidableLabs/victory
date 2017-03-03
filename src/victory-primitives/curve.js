4fca7076e51e3e5311250d5448991a7f554af258
Derek Maffett <derekgmaffett@gmail.com>
2017-03-03 05:57:17 -0800

    refactor data segmentation

--- src/victory-primitives/curve.js
+++ src/victory-primitives/curve.js
@@ -56,23 +56,42 @@ export default class Curve extends React.Component {
   }
 
   getDataSegments(data) {
-    let segmentStartIndex = 0;
-    const segments = data.reduce((memo, datum, index) => {
+    return this.splitArray(data, (datum) => {
       const yDatum = datum.y1 !== undefined ? datum._y1 : datum._y;
-      if (yDatum === null || typeof yDatum === "undefined") {
-        memo = memo.concat([data.slice(segmentStartIndex, index)]);
+
+      return yDatum === null || typeof yDatum === "undefined";
+    }).filter((segment) => segment.length > 1);
+  }
+
+  /**
+   * Split array into subarrays using a delimiter function. Items qualifying as
+   * delimiters are excluded from the subarrays. Functions similarly to String.split
+   *
+   * Example:
+   * const array = [1, 2, 3, "omit", 4, 5, "omit", 6]
+   * splitArray(array, (item) => item === "omit");
+   * => [[1, 2, 3], [4, 5], [6]]
+   *
+   * @param {Array}    array        An array of items
+   * @param {Function} delimiterFn  A function indicating values to be used as delimiters
+   * @returns {Object}              Array of subarrays
+   */
+  splitArray(array, splitValueFn) {
+    let segmentStartIndex = 0;
+    const segments = array.reduce((memo, item, index) => {
+      if (splitValueFn(item)) {
+        memo = memo.concat([array.slice(segmentStartIndex, index)]);
         segmentStartIndex = index + 1;
-      } else if (index === data.length - 1) {
-        memo = memo.concat([data.slice(segmentStartIndex, data.length)]);
+      } else if (index === array.length - 1) {
+        memo = memo.concat([array.slice(segmentStartIndex, array.length)]);
       }
       return memo;
     }, []);
 
     return segments.filter((segment) => {
-      return Array.isArray(segment) && segment.length > 1;
+      return Array.isArray(segment) && segment.length > 0;
     });
   }
-
 
   toNewName(interpolation) {
     // d3 shape changed the naming scheme for interpolators from "basis" -> "curveBasis" etc.
