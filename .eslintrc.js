413858cd3ef95cc69cda883eec34866cb5db6060
Chris Bolin <bolin.chris@gmail.com>
2017-04-23 09:35:29 +0700

    update eslintrc.js to work with victory-multi

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,12 @@
-var path = require("path");
-var base = path.dirname(require.resolve("builder-victory-component/package.json"));
+const path = require("path");
+let base;
+try {
+  // Normal resolution
+  base = path.dirname(require.resolve("builder-victory-component/package.json"));
+} catch (err) {
+  // Dev-only resolution
+  base = path.dirname(require.resolve("../builder-victory-component/package.json"));
+}
 
 module.exports = {
   extends: path.join(base, "config/eslint/.eslintrc-source")
