c9b4dc7630f2c502b927249d31673dec968b9b05
boygirl <lauren.eastridge@formidable.com>
2017-05-04 15:23:09 -0700

    fix arc rendering

--- src/victory-primitives/arc.js
+++ src/victory-primitives/arc.js
@@ -1,4 +1,4 @@
-/*eslint no-magic-numbers: ["error", { "ignore": [2, 180] }]*/
+/*eslint no-magic-numbers: ["error", { "ignore": [0, 1, 2, 180] }]*/
 import React from "react";
 import PropTypes from "prop-types";
 import Helpers from "../victory-util/helpers";
@@ -47,16 +47,18 @@ export default class Arc extends React.Component {
   getArcPath(props) {
     const { cx, cy, r, startAngle, endAngle, closedPath } = props;
     // Always draw the path as two arcs so that complete circles may be rendered.
-    const halfAngle = (endAngle - startAngle) / 2;
+    const halfAngle = (Math.abs(endAngle - startAngle) / 2) + startAngle;
     const x1 = cx + r * Math.sin(this.degreesToRadians(startAngle));
     const y1 = cy + r * Math.cos(this.degreesToRadians(startAngle));
     const x2 = cx + r * Math.sin(this.degreesToRadians(halfAngle));
     const y2 = cy + r * Math.cos(this.degreesToRadians(halfAngle));
     const x3 = cx + r * Math.sin(this.degreesToRadians(endAngle));
     const y3 = cy + r * Math.cos(this.degreesToRadians(endAngle));
+    const largerArcFlag1 = halfAngle - startAngle <= 180 ? 0 : 1;
+    const largerArcFlag2 = endAngle - halfAngle <= 180 ? 0 : 1;
     const arcStart = closedPath ? ` M ${cx}, ${cy} L ${x1}, ${y1}` : `M ${x1}, ${y1}`;
-    const arc1 = `A ${r}, ${r}, 0, 1, 0, ${x2}, ${y2}`;
-    const arc2 = `A ${r}, ${r}, 0, 1, 0, ${x3}, ${y3}`;
+    const arc1 = `A ${r}, ${r}, 0, ${largerArcFlag1}, 0, ${x2}, ${y2}`;
+    const arc2 = `A ${r}, ${r}, 0, ${largerArcFlag2}, 0, ${x3}, ${y3}`;
     const arcEnd = closedPath ? "Z" : "";
     return `${arcStart} ${arc1} ${arc2} ${arcEnd}`;
   }
