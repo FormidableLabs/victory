b1d3f5caa536a8f4ee04b04ebbe83e1d570f9be0
boygirl <lauren.eastridge@formidable.com>
2017-05-09 19:15:58 -0700

    clean up portal

--- src/victory-portal/portal.js
+++ src/victory-portal/portal.js
@@ -37,15 +37,13 @@ export default class Portal extends React.Component {
 
   // Overridden in victory-core-native
   render() {
-    return (
-      <svg width={450} height={300} viewBox="0 0 450 300" overflow="visible">
-        <g>
-          {Object.keys(this.map).map((key) => {
-            const el = this.map[key];
-            return el ? React.cloneElement(el, { key }) : el;
-          })}
-        </g>
-      </svg>
+    return React.cloneElement(
+      this.props.groupComponent,
+      {},
+      Object.keys(this.map).map((key) => {
+        const el = this.map[key];
+        return el ? React.cloneElement(el, { key }) : el;
+      })
     );
   }
 }
