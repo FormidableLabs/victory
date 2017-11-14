b5aef989babd8c5d4a2e7ff8c4bf2ba3f4cc5122
boygirl <lauren.eastridge@formidable.com>
2017-11-14 15:28:15 -0800

    polar bars with corner radius only on top

--- src/victory-primitives/bar.js
+++ src/victory-primitives/bar.js
@@ -157,6 +157,12 @@ export default class Bar extends React.Component {
     }
   }
 
+  getPathMoves(path) {
+    const moves = path.match(/[A-Z]/g);
+    const coords = path.split(/[A-Z]/);
+    return moves.map((m, i) => ({ command: m, coords: coords[i + 1].split(",") }));
+  }
+
   getVerticalPolarBarPath(props) { // eslint-disable-line max-statements
     const { datum, scale, style, index, alignment } = props;
     const r1 = scale.y(datum._y0 || 0);
@@ -178,8 +184,23 @@ export default class Bar extends React.Component {
       .innerRadius(r1)
       .outerRadius(r2)
       .startAngle(this.transformAngle(start))
-      .endAngle(this.transformAngle(end))
-      .cornerRadius(cornerRadius);
+      .endAngle(this.transformAngle(end));
+    if (cornerRadius) {
+      const withCorners = d3Shape.arc()
+        .innerRadius(r1)
+        .outerRadius(r2)
+        .startAngle(this.transformAngle(start))
+        .endAngle(this.transformAngle(end))
+        .cornerRadius(cornerRadius);
+      const moves = this.getPathMoves(path());
+      const cornerMoves = this.getPathMoves(withCorners());
+      // eslint-disable-next-line no-magic-numbers
+      const totalMoves = cornerMoves.slice(0, 4).concat(moves.slice(2));
+      return totalMoves.reduce((memo, move) => {
+        memo += `${move.command} ${move.coords.join()}`;
+        return memo;
+      }, "");
+    }
     return path();
   }
 
