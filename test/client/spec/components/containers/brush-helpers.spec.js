dd62142b06555ce11003fe1a1d3d2d280a411e90
jbockerstette <jim.bockerstette@gmail.com>
2017-12-28 12:21:51 -0800

    Fixed failing date test.

--- test/client/spec/components/containers/brush-helpers.spec.js
+++ test/client/spec/components/containers/brush-helpers.spec.js
@@ -44,8 +44,9 @@ describe("containers/brush-helpers", () => {
     });
 
     it("returns correct box when x axis is dates", () => {
-      const fullDomainBox = { x1: new Date("1/2/2017"), x2: new Date("2/1/2017"), y1: 0, y2: 2 };
-      const box = { x1: new Date("1/1/2017"), x2: new Date("1/10/2017"), y1: 1, y2: 2 };
+      const fullDomainBox = { x1: new Date("1/2/2017 PST"),
+        x2: new Date("2/1/2017 PST"), y1: 0, y2: 2 };
+      const box = { x1: new Date("1/1/2017 PST"), x2: new Date("1/10/2017 PST"), y1: 1, y2: 2 };
       const constrainBoxResult = BrushHelpers.constrainBox(box, fullDomainBox);
       expect(constrainBoxResult).to.eql({ x1: 1483344000000, y1: 1, x2: 1484121600000, y2: 2 });
     });
