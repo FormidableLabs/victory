1f1663d1d74c5fa1729f0c337010f4fa7fa8de6a
boygirl <lauren.eastridge@formidable.com>
2018-01-02 10:48:48 -0800

    add demos

--- demo/components/external-events-demo.js
+++ demo/components/external-events-demo.js
@@ -3,7 +3,8 @@
 
 import React from "react";
 import {
-  VictoryChart, VictoryBar
+  VictoryChart, VictoryBar, VictoryStack, VictoryVoronoiContainer,
+  VictoryLine, VictoryArea, VictoryZoomContainer
 } from "../../src/index";
 import { range } from "lodash";
 
@@ -42,7 +43,7 @@ class App extends React.Component {
     const callback = this.removeMutation.bind(this);
     this.setState({
       externalMutation: [{
-        childName: "bar",
+        childName: "data",
         target: ["data", "labels"],
         eventKey: "all",
         mutation: (props) => {
@@ -61,7 +62,7 @@ class App extends React.Component {
     this.setState({
       externalMutation: [
         {
-          childName: "bar",
+          childName: "data",
           target: "all",
           eventKey: "all",
           mutation: () => ({ style: undefined }),
@@ -79,75 +80,183 @@ class App extends React.Component {
       alignItems: "center",
       justifyContent: "center"
     };
-    const chartStyle = { parent: { border: "1px solid #ccc", margin: "2%", maxWidth: "40%" } };
+    const chartStyle = { parent: { border: "1px solid #ccc", margin: "2%", maxWidth: "35%" } };
     return (
       <div className="demo">
         <h1>Debug</h1>
         <div style={containerStyle}>
-        <ul>
-        <li><button onClick={this.handleClick.bind(this)}>External Mutation</button></li>
-        <li><button onClick={this.clearMutation.bind(this)}>Clear Mutation</button></li>
-        </ul>
-        <VictoryChart style={chartStyle} domainPadding={{ x: 40 }}
-          animate
-          externalEventMutations={this.state.externalMutation}
-          events={[
-            {
-              childName: "bar",
-              target: "data",
-              eventHandlers: {
-                onClick: () => {
-                  return [
-                    {
-                      mutation: () => {
-                        return { style: { fill: "orange" } };
+          <ul>
+            <li><button onClick={this.handleClick.bind(this)}>External Mutation</button></li>
+            <li><button onClick={this.clearMutation.bind(this)}>Clear Mutation</button></li>
+          </ul>
+          <VictoryChart style={chartStyle} domainPadding={{ x: 40 }}
+            animate
+            externalEventMutations={this.state.externalMutation}
+            events={[
+              {
+                childName: "data",
+                target: "data",
+                eventHandlers: {
+                  onClick: () => {
+                    return [
+                      {
+                        mutation: () => {
+                          return { style: { fill: "orange" } };
+                        }
+                      }, {
+                        target: "labels",
+                        mutation: (props) => {
+                          return props.text === "clicked" ?
+                            { text: "unclicked" } : { text: "clicked" };
+                        }
                       }
-                    }, {
-                      target: "labels",
-                      mutation: (props) => {
-                        return props.text === "clicked" ? { text: "unclicked" } : { text: "clicked" };
-                      }
-                    }
-                  ];
+                    ];
+                  }
                 }
               }
-            }
-          ]}
-        >
-          <VictoryBar
-            name="bar"
-            data={this.state.data}
-            style={{ data: { fill: "cyan" } }}
-          />
-        </VictoryChart>
+            ]}
+          >
+            <VictoryBar
+              name="data"
+              data={this.state.data}
+              style={{ data: { fill: "cyan" } }}
+            />
+          </VictoryChart>
 
-        <VictoryBar style={chartStyle}
-          animate
-          data={this.state.data}
-          externalEventMutations={this.state.externalMutation}
-          events={[
-            {
-              target: "data",
-              eventHandlers: {
-                onClick: () => {
-                  return [
-                    {
-                      mutation: () => {
-                        return { style: { fill: "orange" } };
+          <VictoryBar style={chartStyle}
+            animate
+            data={this.state.data}
+            externalEventMutations={this.state.externalMutation}
+            events={[
+              {
+                target: "data",
+                eventHandlers: {
+                  onClick: () => {
+                    return [
+                      {
+                        mutation: () => {
+                          return { style: { fill: "orange" } };
+                        }
+                      }, {
+                        target: "labels",
+                        mutation: (props) => {
+                          return props.text === "clicked" ?
+                            { text: "unclicked" } : { text: "clicked" };
+                        }
                       }
-                    }, {
-                      target: "labels",
-                      mutation: (props) => {
-                        return props.text === "clicked" ?
-                          { text: "unclicked" } : { text: "clicked" };
-                      }
-                    }
-                  ];
+                    ];
+                  }
                 }
               }
-            }
-          ]}
-        />
+            ]}
+          />
+          <VictoryChart style={chartStyle}
+            externalEventMutations={this.state.externalMutation}
+            events={[
+              {
+                childName: "data",
+                target: "data",
+                eventHandlers: {
+                  onClick: () => {
+                    return [
+                      {
+                        mutation: () => {
+                          return { style: { fill: "orange" } };
+                        }
+                      }, {
+                        target: "labels",
+                        mutation: (props) => {
+                          return props.text === "clicked" ?
+                            { text: "unclicked" } : { text: "clicked" };
+                        }
+                      }
+                    ];
+                  }
+                }
+              }
+            ]}
+          >
+            <VictoryStack
+              colorScale={"qualitative"}
+            >
+              <VictoryBar
+                data={[
+                  { x: 1, y: 1 },
+                  { x: 2, y: 2 },
+                  { x: 3, y: 5 }
+                ]}
+              />
+              <VictoryBar
+                data={[
+                  { x: 1, y: 2 },
+                  { x: 2, y: 1 },
+                  { x: 3, y: 7 }
+                ]}
+              />
+              <VictoryBar name="data"
+                data={[
+                  { x: 1, y: 3 },
+                  { x: 2, y: 4 },
+                  { x: 3, y: 9 }
+                ]}
+              />
+            </VictoryStack>
+          </VictoryChart>
+          <VictoryStack style={chartStyle}
+            externalEventMutations={this.state.externalMutation}
+            containerComponent={<VictoryVoronoiContainer/>}
+          >
+            <VictoryBar name="data"
+              style={{
+                data: {
+                  fill: "tomato",
+                  stroke: (d, active) => active ? "black" : "none",
+                  strokeWidth: 2
+                }
+              }}
+              size={(datum, active) => active ? 5 : 3}
+              data={[
+                { x: 1, y: -5 },
+                { x: 2, y: 4 },
+                { x: 3, y: 2 },
+                { x: 4, y: 3 },
+                { x: 5, y: 1 },
+                { x: 6, y: -3 },
+                { x: 7, y: 3 }
+              ]}
+            />
+            <VictoryBar
+              style={{
+                data: {
+                  fill: "orange",
+                  stroke: (d, active) => active ? "black" : "none",
+                  strokeWidth: 2
+                }
+              }}
+              size={(datum, active) => active ? 5 : 3}
+              data={[
+                { x: 1, y: -3 },
+                { x: 2, y: 5 },
+                { x: 3, y: 3 },
+                { x: 4, y: 0 },
+                { x: 5, y: -2 },
+                { x: 6, y: -2 },
+                { x: 7, y: 5 }
+              ]}
+            />
+          </VictoryStack>
+          <VictoryChart
+            style={chartStyle}
+            containerComponent={<VictoryZoomContainer/>}
+            externalEventMutations={this.state.externalMutation}
+          >
+            <VictoryArea name="data"
+              style={{ data: { stroke: "#333", fill: "#888", opacity: 0.4 } }}
+              data={this.state.data}
+              interpolation="stepBefore"
+            />
+            <VictoryLine data={this.state.data} interpolation="stepBefore"/>
+          </VictoryChart>
         </div>
       </div>
     );
