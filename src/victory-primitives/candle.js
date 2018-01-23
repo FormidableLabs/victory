923398f5501c691614cf1949ad1e75806a5ca7bc
Kylie Stewart <kylie.stewart@me.com>
2018-01-23 11:15:42 -0700

    lint success!

--- src/victory-primitives/candle.js
+++ src/victory-primitives/candle.js
@@ -89,9 +89,9 @@ export default class Candle extends React.Component {
     const role = props.role || "presentation";
     const wickStyle = assign(
       {}, this.style, { strokeWidth: props.style.wickStrokeWidth || props.style.strokeWidth });
-    return assign({ 
-      x1: x, x2: x, y1: wickType === "low" ? lowWick : y1, y2: wickType === "high" ? highWick : y2, 
-      style: wickStyle, role, shapeRendering, className 
+    return assign({
+      x1: x, x2: x, y1: wickType === "low" ? lowWick : y1, y2: wickType === "high" ? highWick : y2,
+      style: wickStyle, role, shapeRendering, className
     }, events);
   }
 
@@ -100,10 +100,10 @@ export default class Candle extends React.Component {
     const highWickProps = this.getWickProps(this.props, "high");
     const lowWickProps = this.getWickProps(this.props, "low");
     return React.cloneElement(
-      this.props.groupComponent, 
-      {}, 
-      this.renderWick(highWickProps), 
-      this.renderWick(lowWickProps), 
+      this.props.groupComponent,
+      {},
+      this.renderWick(highWickProps),
+      this.renderWick(lowWickProps),
       this.renderCandle(candleProps)
   );
   }
