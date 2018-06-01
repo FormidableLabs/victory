2b8d21cc2914392cf2c6a3c349b8fb17e8b1a6c0
boygirl <lauren.eastridge@formidable.com>
2018-06-01 12:30:25 -0700

    start rewriting domain spec

--- test/client/spec/victory-util/domain.spec.js
+++ test/client/spec/victory-util/domain.spec.js
@@ -1,8 +1,7 @@
 /* eslint no-unused-expressions: 0 */
 /* eslint max-nested-callbacks: 0 */
-/* global sinon */
 
-import { Data, Domain, Helpers } from "src/index";
+import { Domain } from "src/index";
 
 describe("helpers/domain", () => {
   describe("padDomain", () => {
@@ -29,21 +28,9 @@ describe("helpers/domain", () => {
   });
 
   describe("getDomain", () => {
-    let sandbox;
-    beforeEach(() => {
-      sandbox = sinon.sandbox.create();
-      sandbox.spy(Domain, "getDomainFromProps");
-      sandbox.spy(Domain, "getDomainFromData");
-    });
-    afterEach(() => {
-      sandbox.restore();
-    });
-
     it("gets the domain from props if they exist", () => {
       const props = { domain: [1, 2] };
       const resultDomain = Domain.getDomain(props, "x");
-      expect(Domain.getDomainFromProps).calledWith(props, "x").and.returned(props.domain);
-      expect(Domain.getDomainFromData).not.called;
       expect(resultDomain).to.eql(props.domain);
     });
 
@@ -52,9 +39,6 @@ describe("helpers/domain", () => {
         x: "x", y: "y", domain: { y: [1, 2] }, data: [{ x: 1, y: 3 }, { x: 3, y: 5 }]
       };
       const resultDomain = Domain.getDomain(props, "x");
-      const dataset = Data.getData(props);
-      expect(Domain.getDomainFromProps).calledWith(props, "x").and.returned(undefined);
-      expect(Domain.getDomainFromData).calledWith(props, "x", dataset).and.returned([1, 3]);
       expect(resultDomain).to.eql([1, 3]);
     });
   });
@@ -81,6 +65,51 @@ describe("helpers/domain", () => {
       const resultDomain = Domain.getDomainFromProps(props, "x");
       expect(resultDomain).to.eql(undefined);
     });
+
+    it("returns a domain from minDomain and maxDomain if both are defined", () => {
+      const props = { minDomain: 1, maxDomain: 10 };
+      const resultDomain = Domain.getDomainFromProps(props, "x");
+      expect(resultDomain).to.eql([1, 10]);
+    });
+
+    it("returns an adjusted domain if minDomain equals maxDomain", () => {
+      const props = { minDomain: 0, maxDomain: 0 };
+      const verySmallNumber = Math.pow(10, -10);
+      const resultDomain = Domain.getDomainFromProps(props, "x");
+      expect(resultDomain).to.eql([-1 * verySmallNumber, verySmallNumber]);
+    });
+
+    it("returns undefined if only minDomain is defined", () => {
+      const props = { minDomain: 1 };
+      const resultDomain = Domain.getDomainFromProps(props, "x");
+      expect(resultDomain).to.eql(undefined);
+    });
+  });
+
+  describe("getDomainFromMinMax", () => {
+    it("returns a min max array when min and max are given", () => {
+      const min = 1;
+      const max = 2;
+      const resultDomain = Domain.getDomainFromMinMax(min, max);
+      expect(resultDomain).to.eql([min, max]);
+    });
+
+    it("returns an adjusted domain if min equals max", () => {
+      const min = 0;
+      const max = 0;
+      const verySmallNumber = Math.pow(10, -10);
+      const resultDomain = Domain.getDomainFromMinMax(min, max);
+      expect(resultDomain).to.eql([-1 * verySmallNumber, verySmallNumber]);
+    });
+
+    it("returns an adjusted date domain if min equals max", () => {
+      const min = new Date(1980, 1, 1);
+      const max = new Date(1980, 1, 1);
+      const resultDomain = Domain.getDomainFromMinMax(min, max);
+      expect(resultDomain).to.eql([
+        new Date(+min - 1), new Date(+max + 1)
+      ]);
+    });
   });
 
   describe("getDomainFromData", () => {
@@ -92,55 +121,26 @@ describe("helpers/domain", () => {
   });
 
   describe("getDomainFromTickValues", () => {
-    let sandbox;
-    beforeEach(() => {
-      sandbox = sinon.sandbox.create();
-      sandbox.spy(Helpers, "isVertical");
-      sandbox.spy(Helpers, "stringTicks");
-    });
-
-    afterEach(() => {
-      sandbox.restore();
-    });
-
     it("determines a domain from tickValues", () => {
       const props = { tickValues: [1, 2, 3] };
       const domainResult = Domain.getDomainFromTickValues(props);
-      expect(Helpers.stringTicks).calledWith(props).and.returned(false);
-      expect(Helpers.isVertical).calledWith(props).and.returned(false);
       expect(domainResult).to.eql([1, 3]);
     });
 
     it("determines a domain from string tick values", () => {
       const props = { tickValues: ["a", "b", "c", "d"] };
       const domainResult = Domain.getDomainFromTickValues(props);
-      expect(Helpers.stringTicks).calledWith(props).and.returned(true);
-      expect(Helpers.isVertical).calledWith(props).and.returned(false);
       expect(domainResult).to.eql([1, 4]);
     });
 
     it("reverses a domain from tickValues when the axis is vertical", () => {
       const props = { tickValues: [1, 2, 3], dependentAxis: true };
       const domainResult = Domain.getDomainFromTickValues(props);
-      expect(Helpers.stringTicks).calledWith(props).and.returned(false);
-      expect(Helpers.isVertical).calledWith(props).and.returned(true);
       expect(domainResult).to.eql([3, 1]);
     });
   });
 
   describe("getDomainFromGroupedData", () => {
-    let sandbox;
-    beforeEach(() => {
-      sandbox = sinon.sandbox.create();
-      sandbox.spy(Domain, "getDomainFromData");
-      sandbox.spy(Domain, "getDomainFromCategories");
-      sandbox.spy(Domain, "getCumulativeData");
-    });
-
-    afterEach(() => {
-      sandbox.restore();
-    });
-
     const data = [
       [{ _x: 1, _y: 0 }, { _x: 2, _y: 0 }, { _x: 3, _y: 0 }],
       [{ _x: 1, _y: 1 }, { _x: 2, _y: 1 }, { _x: 3, _y: 1 }],
@@ -150,17 +150,12 @@ describe("helpers/domain", () => {
     it("calculates a domain from categories for the independent axis", () => {
       const props = { categories: [1, 2, 3], data, x: "x", y: "y" };
       const domainResultX = Domain.getDomainFromGroupedData(props, "x", data);
-      expect(Domain.getDomainFromCategories).calledWith(props, "x").and.returned([1, 3]);
-      expect(Domain.getDomainFromData).not.called;
-      expect(Domain.getCumulativeData).not.called;
       expect(domainResultX).to.eql([1, 3]);
     });
 
     it("calculates a stacked domain for the dependent axis", () => {
       const props = { categories: [1, 2, 3], data, x: "x", y: "y" };
       const domainResultY = Domain.getDomainFromGroupedData(props, "y", data);
-      expect(Domain.getDomainFromData).calledOnce.and.returned([0, 2]);
-      expect(Domain.getCumulativeData).calledOnce;
       expect(domainResultY).to.eql([0, 3]);
     });
   });
