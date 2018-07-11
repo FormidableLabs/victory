94c552944b129eeab0590edc9b309f5a9cb1d805
boygirl <lauren.eastridge@formidable.com>
2018-07-11 12:51:41 -0700

    clean up

--- perf/client/bench/components/victory-axis/sample-data.js
+++
@@ -1,5 +0,0 @@
-import generator from "../../helpers/sample-data-generator";
-
-export const tickValueSamples = generator.samples([10, 50, 100],
-  () => generator.string(1, 7)
-);
--- perf/client/main.js
+++
@@ -1,3 +0,0 @@
-// Use webpack to infer and `require` tests automatically.
-const benchesReq = require.context(".", true, /\.bench.js?$/);
-benchesReq.keys().map(benchesReq);
