55b17f47a98153d911f328285368e7d1f678a0f9
boygirl <lauren.eastridge@formidable.com>
2018-07-11 23:03:11 -0700

    add watch script

--- package-scripts.js
+++ package-scripts.js
@@ -6,7 +6,6 @@ module.exports = {
     "clean-lib": "rimraf lib",
     "clean-es": "rimraf es",
     "clean-dist": "rimraf dist",
-    "clean-all": npsUtils.series.nps("clean-lib", "clean-es", "clean-dist"),
     "babel-es": "cross-env BABEL_ENV=es babel src --out-dir es --copy-files",
     "babel-lib": "cross-env BABEL_ENV=commonjs babel src --out-dir lib --copy-files",
     "build-es": npsUtils.series.nps("clean-es", "babel-es"),
@@ -16,6 +15,8 @@ module.exports = {
     "build-dist-min": "webpack --bail --config ../../config/webpack/webpack.config.js --colors",
     "build-dists": npsUtils.concurrent.nps("build-dist-min", "build-dist-dev"),
     "build-dist": npsUtils.series.nps("clean-dist", "build-dists"),
-    "build-all": npsUtils.concurrent.nps("build-libs", "build-dist")
+    "watch-babel-es": "cross-env BABEL_ENV=es babel src --out-dir es --copy-files --watch",
+    "watch-babel-lib": "cross-env BABEL_ENV=lib babel src --out-dir lib --copy-files --watch",
+    "watch-build": npsUtils.concurrent.nps("watch-babel-es", "watch-babel-lib")
   }
-}
+};
