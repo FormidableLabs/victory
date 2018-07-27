0fce371ff98a81ba4deb7adf6eddac6ac3390774
boygirl <lauren.eastridge@formidable.com>
2018-07-26 23:33:12 -0700

    undo demo change

--- demo/components/victory-area-demo.js
+++ demo/components/victory-area-demo.js
@@ -191,7 +191,6 @@ export default class App extends React.Component {
           />
         </VictoryStack>
 
-        <VictoryChart style={style}>
         <VictoryGroup
           style={{
             parent: style.parent,
@@ -207,7 +206,6 @@ export default class App extends React.Component {
             data={[{ x: 1, y: 3 }, { x: 2, y: 2 }, { x: 3, y: 6 }, { x: 4, y: 2 }, { x: 5, y: 6 }]}
           />
         </VictoryGroup>
-        </VictoryChart>
 
         <VictoryStack
           style={{ parent: style.parent }}
