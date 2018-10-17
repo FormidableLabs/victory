dea8d85a735093b59af7077f20ac11469aa8e0b8
Narin Luangrath <narin.luangrath@formidable.com>
2018-10-17 15:54:56 -0700

    Export useful path helper methods

--- packages/victory-bar/src/index.js
+++ packages/victory-bar/src/index.js
@@ -1,2 +1,7 @@
 export { default as VictoryBar } from "./victory-bar";
 export { default as Bar } from "./bar";
+export {
+  getVerticalBarPath,
+  getHorizontalBarPath,
+  getVerticalPolarBarPath
+} from "./geometry-helper-methods"
