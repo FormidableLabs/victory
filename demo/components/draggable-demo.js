d17971bb9436fe53ba4ec3c53c5113641b605e53
boygirl <lauren.eastridge@formidable.com>
2019-02-02 16:12:36 -0800

    lint

--- demo/components/draggable-demo.js
+++ demo/components/draggable-demo.js
@@ -1,4 +1,4 @@
-/*eslint-disable no-magic-numbers */
+/*eslint-disable no-magic-numbers,react/no-multi-comp */
 
 import React from "react";
 import { VictoryChart } from "../../packages/victory-chart/src/index";
