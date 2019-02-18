3722ae55b282102bcf78e49536939bd470cab145
boygirl <lauren.eastridge@formidable.com>
2019-02-18 15:15:34 -0800

    tweak demo

--- demo/components/horizontal-demo.js
+++ demo/components/horizontal-demo.js
@@ -163,24 +163,24 @@ class App extends React.Component {
             { x: new Date(2015, 1, 1), y: 470 }
           ]}
         />
-
-        <VictoryLine horizontal
-          style={{
-            parent: chartStyle.parent,
-            data: { stroke: "red", strokeWidth: 2 }
-          }}
-          labels={() => "yo"}
-          data={[
-            { x: new Date(1982, 1, 1), y: 125 },
-            { x: new Date(1987, 1, 1), y: 257 },
-            { x: new Date(1993, 1, 1), y: 345 },
-            { x: new Date(1997, 1, 1), y: 515 },
-            { x: new Date(2001, 1, 1), y: null },
-            { x: new Date(2005, 1, 1), y: 305 },
-            { x: new Date(2011, 1, 1), y: 270 },
-            { x: new Date(2015, 1, 1), y: 470 }
-          ]}
-        />
+        <VictoryChart horizontal style={chartStyle}>
+          <VictoryLine
+            style={{
+              data: { stroke: "red", strokeWidth: 2 }
+            }}
+            labels={() => "yo"}
+            data={[
+              { x: new Date(1982, 1, 1), y: 125 },
+              { x: new Date(1987, 1, 1), y: 257 },
+              { x: new Date(1993, 1, 1), y: 345 },
+              { x: new Date(1997, 1, 1), y: 515 },
+              { x: new Date(2001, 1, 1), y: null },
+              { x: new Date(2005, 1, 1), y: 305 },
+              { x: new Date(2011, 1, 1), y: 270 },
+              { x: new Date(2015, 1, 1), y: 470 }
+            ]}
+          />
+        </VictoryChart>
         <VictoryBar horizontal
           style={chartStyle}
           labels={() => "yo"}
