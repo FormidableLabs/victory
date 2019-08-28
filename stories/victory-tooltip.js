9d65b42477b71147def3d7e43b49427ffc58d097
boygirl <lauren.eastridge@formidable.com>
2019-08-27 20:18:16 -0700

    add cornerRadius regression story

--- stories/victory-tooltip.js
+++ stories/victory-tooltip.js
@@ -153,3 +153,33 @@ storiesOf("VictoryTooltip.flyoutWidth", module)
       }
     />
   ));
+
+storiesOf("VictoryTooltip.cornerRadius", module)
+  .addDecorator(
+    getChartDecorator({
+      domainPadding: 25,
+      style: { parent: { border: "1px solid #ccc" } }
+    })
+  )
+  .add("= 0", () => (
+    <VictoryBar
+      data={getMixedData(5)}
+      labels={({ datum }) => `x: ${datum.x}`}
+      labelComponent={<VictoryTooltip active dy={0} cornerRadius={0} />}
+    />
+  ))
+  .add("= 10", () => (
+    <VictoryBar
+      horizontal
+      data={getMixedData(5)}
+      labels={({ datum }) => `x: ${datum.x}`}
+      labelComponent={<VictoryTooltip active dx={0} cornerRadius={10} />}
+    />
+  ))
+  .add("as a function", () => (
+    <VictoryBar
+      data={getMixedData(5)}
+      labels={({ datum }) => `x: ${datum.x}`}
+      labelComponent={<VictoryTooltip active dy={0} cornerRadius={({ datum }) => datum.x * 2} />}
+    />
+  ));
