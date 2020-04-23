1b2df57d97aeb901fd90f5f6f610afeb9410f3e5
Kylie Stewart <kylie@kyliestewart.tech>
2020-04-23 12:35:00 -0600

    Remove scattered credits & centralize to the LICENSE

--- packages/victory-legend/src/index.d.ts
+++ packages/victory-legend/src/index.d.ts
@@ -1,14 +1,3 @@
-// Definitions by: Alexey Svetliakov <https://github.com/asvetliakov>
-//                 snerks <https://github.com/snerks>
-//                 Krzysztof Cebula <https://github.com/Havret>
-//                 Vitaliy Polyanskiy <https://github.com/alreadyExisted>
-//                 James Lismore <https://github.com/jlismore>
-//                 Stack Builders <https://github.com/stackbuilders>
-//                 Esteban Ibarra <https://github.com/ibarrae>
-//                 Dominic Lee <https://github.com/dominictwlee>
-//                 Dave Vedder <https://github.com/veddermatic>
-//                 Alec Flett <https://github.com/alecf>
-
 import * as React from "react";
 import {
   BlockProps,
