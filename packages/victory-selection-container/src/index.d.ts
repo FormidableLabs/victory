cdc031555bcebe1d3f04691a1824f52ba56354e5
Kylie Stewart <kylie@kyliestewart.tech>
2020-04-24 16:33:44 -0600

    Explicitly define funcs

--- packages/victory-selection-container/src/index.d.ts
+++ packages/victory-selection-container/src/index.d.ts
@@ -1,12 +1,22 @@
 import * as React from "react";
 import { VictoryContainerProps } from "victory-core";
 
+interface PointsInterface {
+  childName?: string | string[];
+  eventKey?: string | number;
+  data?: any;
+}
+
 interface VictorySelectionContainerProps extends VictoryContainerProps {
   activateSelectedData?: boolean;
   allowSelection?: boolean;
   disable?: boolean;
-  onSelection?: Function;
-  onSelectionCleared?: Function;
+  onSelection?: (
+    points: PointsInterface[],
+    bounds: { x: number | Date; y: number | Date }[],
+    props: VictorySelectionContainerProps
+  ) => void;
+  onSelectionCleared?: (props: VictorySelectionContainerProps) => void;
   selectionBlacklist?: string[];
   selectionComponent?: React.ReactElement;
   selectionDimension?: "x" | "y";
