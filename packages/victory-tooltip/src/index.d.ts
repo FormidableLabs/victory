d0412cb10a57db6f04991ce27982cb3d04dcef55
Madeline Harris <m.harris02@gmail.com>
2020-04-27 17:10:06 +0100

    Add width to VictoryTooltip props

--- packages/victory-tooltip/src/index.d.ts
+++ packages/victory-tooltip/src/index.d.ts
@@ -42,6 +42,7 @@ export interface VictoryTooltipProps extends VictoryLabelableProps {
   renderInPortal?: boolean;
   style?: React.CSSProperties;
   text?: StringOrNumberOrCallback | string[] | number[];
+  width?: number;
   x?: number;
   y?: number;
 }
