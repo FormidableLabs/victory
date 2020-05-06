e0e0f1f3382b7e2f31a962e1f18953feb6c14d1e
Madeline Harris <m.harris02@gmail.com>
2020-05-07 00:30:30 +0100

    Remove an unused import in Background, alphebetise props

--- packages/victory-core/src/victory-primitives/background.js
+++ packages/victory-core/src/victory-primitives/background.js
@@ -1,38 +1,35 @@
 import React from "react";
 import PropTypes from "prop-types";
-import Helpers from "../victory-util/helpers";
 import CommonProps from "../victory-util/common-props";
 import Rect from "./rect";
 import Circle from "./circle";
 
 const getRangeBounds = (props) => ({ x: props.range.x[0], y: props.range.y[1] });
-const getHeight = (props) =>  props.polar
-  ? props.range.y[1]
-  : props.range.y[0] - props.range.y[1];
+const getHeight = (props) => (props.polar ? props.range.y[1] : props.range.y[0] - props.range.y[1]);
 const getWidth = (props) => props.range.x[1] - props.range.x[0];
 
 const Background = (props) => {
   return props.polar
-  ? React.cloneElement(props.circleComponent, {
-    ...props.events,
-    style: props.style && props.style.background,
-    role: props.role,
-    shapeRendering: props.shapeRendering,
-    cx: '50%',
-    cy: '50%',
-    r: getHeight(props)
-  })
-  : React.cloneElement(props.rectComponent, {
-    ...props.events,
-    style: props.style && props.style.background,
-    role: props.role,
-    shapeRendering: props.shapeRendering,
-    x: getRangeBounds(props).x,
-    y: getRangeBounds(props).y,
-    width: getWidth(props),
-    height: getHeight(props)
-  })
- };
+    ? React.cloneElement(props.circleComponent, {
+        ...props.events,
+        style: props.style && props.style.background,
+        role: props.role,
+        shapeRendering: props.shapeRendering,
+        cx: "50%",
+        cy: "50%",
+        r: getHeight(props)
+      })
+    : React.cloneElement(props.rectComponent, {
+        ...props.events,
+        style: props.style && props.style.background,
+        role: props.role,
+        shapeRendering: props.shapeRendering,
+        x: getRangeBounds(props).x,
+        y: getRangeBounds(props).y,
+        width: getWidth(props),
+        height: getHeight(props)
+      });
+};
 
 Background.propTypes = {
   ...CommonProps.baseProps,
@@ -43,13 +40,13 @@ Background.propTypes = {
       y: PropTypes.arrayOf(PropTypes.string)
     })
   ]),
+  circleComponent: PropTypes.element,
   domain: PropTypes.shape({
     x: PropTypes.arrayOf(PropTypes.number),
     y: PropTypes.arrayOf(PropTypes.number)
   }),
-  rectComponent: PropTypes.element,
-  circleComponent: PropTypes.element,
   horizontal: PropTypes.bool,
+  rectComponent: PropTypes.element,
   role: PropTypes.string,
   shapeRendering: PropTypes.string,
   stringMap: PropTypes.shape({
