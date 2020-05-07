21af3ccc342a5831b3d4a1ae15b5fbab3669b62e
Alex Ngo <alex_ngo@live.com>
2020-05-07 10:56:21 -0700

    add unit tests

--- test/client/spec/victory-histogram/victory-histogram.spec.js
+++ test/client/spec/victory-histogram/victory-histogram.spec.js
@@ -9,97 +9,78 @@ import React from "react";
 import { shallow, mount } from "enzyme";
 import { omit, range } from "lodash";
 import SvgTestHelper from "../svg-test-helper";
-import { VictoryBar, Bar } from "packages/victory-bar/src/index";
+import { VictoryHistogram } from "packages/victory-histogram/src/index";
+import { Bar } from "packages/victory-bar/src/index";
 
-describe("components/victory-bar", () => {
+describe("components/victory-histogram", () => {
   describe("default component rendering", () => {
     it("renders an svg with the correct width and height", () => {
-      const wrapper = mount(<VictoryBar />);
+      const wrapper = mount(<VictoryHistogram />);
       const svg = wrapper.find("svg").at(0);
       expect(svg.prop("style").width).to.equal("100%");
       expect(svg.prop("style").height).to.equal("auto");
     });
 
     it("renders an svg with the correct viewBox", () => {
-      const wrapper = mount(<VictoryBar />);
+      const wrapper = mount(<VictoryHistogram />);
       const svg = wrapper.find("svg").at(0);
       const viewBoxValue = `0 0 ${450} ${300}`;
       expect(svg.prop("viewBox")).to.equal(viewBoxValue);
     });
 
+    it("renders 0 bars", () => {
+      const wrapper = shallow(<VictoryHistogram />);
+      const bars = wrapper.find(Bar);
+      expect(bars.length).to.equal(0);
+    });
+
     it("renders 4 bars", () => {
-      const wrapper = shallow(<VictoryBar />);
+      const wrapper = shallow(<VictoryHistogram bins={[0, 10, 40, 50, 100]} />);
       const bars = wrapper.find(Bar);
       expect(bars.length).to.equal(4);
     });
 
     it("renders each bar as a rectangle", () => {
-      const wrapper = mount(<VictoryBar />);
+      const wrapper = mount(<VictoryHistogram bins={[0, 10, 40, 50]} />);
       const bars = wrapper.find(Bar);
       bars.forEach(SvgTestHelper.expectIsRectangular);
     });
   });
 
   describe("rendering data", () => {
-    it("renders bars for {x, y} shaped data (default)", () => {
-      const data = range(10).map((i) => ({ x: i, y: i }));
-      const wrapper = shallow(<VictoryBar data={data} />);
+    it("renders bars for {x} shaped data (default)", () => {
+      const data = range(10).map((i) => ({ x: i }));
+      const wrapper = shallow(<VictoryHistogram data={data} />);
       const bars = wrapper.find(Bar);
-      expect(bars.length).to.equal(10);
-    });
-
-    it("renders ordered bars when sortKey is passed", () => {
-      const data = range(5)
-        .map((i) => ({ x: i, y: i }))
-        .reverse();
-      const wrapper = shallow(<VictoryBar data={data} sortKey="x" />);
-      const xValues = wrapper.find(Bar).map((bar) => bar.prop("datum")._x);
-      expect(xValues).to.eql([0, 1, 2, 3, 4]);
-    });
-
-    it("renders reverse ordered bars when sortOrder is descending", () => {
-      const data = range(5)
-        .map((i) => ({ x: i, y: i }))
-        .reverse();
-      const wrapper = shallow(<VictoryBar data={data} sortKey="x" sortOrder="descending" />);
-      const xValues = wrapper.find(Bar).map((bar) => bar.prop("datum")._x);
-      expect(xValues).to.eql([4, 3, 2, 1, 0]);
-    });
-
-    it("renders bars for array-shaped data", () => {
-      const data = range(20).map((i) => [i, i]);
-      const wrapper = shallow(<VictoryBar data={data} x={0} y={1} />);
-      const bars = wrapper.find(Bar);
-      expect(bars.length).to.equal(20);
+      expect(bars.length).to.be.greaterThan(0);
     });
 
     it("renders bars for deeply-nested data", () => {
-      const data = range(40).map((i) => ({ a: { b: [{ x: i, y: i }] } }));
-      const wrapper = shallow(<VictoryBar data={data} x="a.b[0].x" y="a.b[0].y" />);
+      const data = range(10).map((i) => ({ a: { b: [{ x: i }] } }));
+      const wrapper = shallow(<VictoryHistogram data={data} x="a.b[0].x" />);
       const bars = wrapper.find(Bar);
-      expect(bars.length).to.equal(40);
+      expect(bars.length).to.be.greaterThan(0);
     });
 
     it("renders bars values with null accessor", () => {
       const data = range(30);
-      const wrapper = shallow(<VictoryBar data={data} x={null} y={null} />);
+      const wrapper = shallow(<VictoryHistogram data={data} x={null} y={null} />);
       const bars = wrapper.find(Bar);
-      expect(bars.length).to.equal(30);
+      expect(bars.length).to.be.greaterThan(0);
     });
 
     it("renders bars with appropriate relative heights", () => {
-      const wrapper = mount(<VictoryBar data={[{ x: 1, y: 1 }, { x: 2, y: 2 }, { x: 3, y: 3 }]} />);
+      const wrapper = mount(
+        <VictoryHistogram
+          data={[{ x: 1 }, { x: 2 }, { x: 2 }, { x: 3 }, { x: 3 }, { x: 3 }]}
+          bins={[1, 2, 3, 4]}
+        />
+      );
       const bars = wrapper.find(Bar);
       const heights = bars.map(SvgTestHelper.getBarHeight);
 
       expect(heights[1] / 2).to.be.closeTo(heights[0], 0.5);
-      expect((heights[2] / 3) * 2).to.be.closeTo(heights[1], 0.5);
-    });
-
-    it("does not render data with null x or y values", () => {
-      const data = [{ x: 1, y: 2 }, { x: null, y: 4 }, { x: 5, y: null }];
-      const wrapper = mount(<VictoryBar data={data} />);
-      expect(wrapper.find(Bar).length).to.equal(1);
+      expect((heights[2] * 2) / 3).to.be.closeTo(heights[1], 0.5);
     });
   });
 
@@ -107,7 +88,7 @@ describe("components/victory-bar", () => {
     it("attaches an event to the parent svg", () => {
       const clickHandler = sinon.spy();
       const wrapper = mount(
-        <VictoryBar
+        <VictoryHistogram
           events={[
             {
               target: "parent",
@@ -126,7 +107,9 @@ describe("components/victory-bar", () => {
     it("attaches an event to data", () => {
       const clickHandler = sinon.spy();
       const wrapper = mount(
-        <VictoryBar
+        <VictoryHistogram
+          data={[{ x: 1 }, { x: 1 }, { x: 2 }]}
+          bins={[1, 2, 3]}
           events={[
             {
               target: "data",
@@ -135,7 +118,9 @@ describe("components/victory-bar", () => {
           ]}
         />
       );
+
       const Data = wrapper.find(Bar);
+      expect(Data).to.have.lengthOf(2);
       Data.forEach((node, index) => {
         const initialProps = Data.at(index).props();
         node.simulate("click");
@@ -147,31 +132,31 @@ describe("components/victory-bar", () => {
         expect(`${clickHandler.args[index][2]}`).to.eql(`${index}`);
       });
     });
+
     it("attaches an event to a label", () => {
       const clickHandler = sinon.spy();
-      const data = [
-        { x: 0, y: 0, label: "0" },
-        { x: 1, y: 1, label: "1" },
-        { x: 2, y: 2, label: "2" }
-      ];
+      const data = [{ x: 0 }, { x: 1 }, { x: 2 }];
       const wrapper = mount(
-        <VictoryBar
+        <VictoryHistogram
           data={data}
+          bins={[0, 1, 2, 3]}
           events={[
             {
               target: "labels",
               eventHandlers: { onClick: clickHandler }
             }
           ]}
+          labels={({ datum }) => `${datum.index}`}
         />
       );
+
       const Labels = wrapper.find("VictoryLabel");
       expect(Labels).to.have.lengthOf(3);
       Labels.forEach((node, index) => {
         node.childAt(0).simulate("click");
         expect(clickHandler).called;
         // the first argument is the standard evt object
-        expect(clickHandler.args[index][1]).to.contain({ text: `${index}` });
+        expect(typeof clickHandler.args[index][1].text === "function").to.eql(true);
         expect(`${clickHandler.args[index][2]}`).to.eql(`${index}`);
       });
     });
@@ -179,8 +164,8 @@ describe("components/victory-bar", () => {
 
   describe("accessibility", () => {
     it("adds an aria role to each bar in the series", () => {
-      const data = range(20).map((y, x) => ({ x, y }));
-      const wrapper = mount(<VictoryBar data={data} />);
+      const data = range(20).map((x) => ({ x }));
+      const wrapper = mount(<VictoryHistogram data={data} />);
 
       wrapper.find("path").forEach((p) => {
         const roleValue = p.prop("role");
