22ca3e75eb584b22df63f67d4172d63a69047d6e
boygirl <lauren.eastridge@formidable.com>
2020-05-22 17:30:08 -0700

    typo

--- .storybook/config.js
+++ .storybook/config.js
@@ -23,7 +23,7 @@ const loadStories = () => {
   require("../stories/victory-chart");
   require("../stories/victory-errorbar");
   require("../stories/victory-histogram");
-  require("../stories/victory-line");y
+  require("../stories/victory-line");
   require("../stories/victory-polar-axis");
   require("../stories/victory-scatter");
   require("../stories/containers-and-addons");
