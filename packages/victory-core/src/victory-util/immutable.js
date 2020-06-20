6891d472e22ab88aa43cdfd2cf707bf7658a2a93
boygirl <lauren.eastridge@formidable.com>
2020-06-20 14:54:36 -0700

    prettier

--- packages/victory-core/src/victory-util/immutable.js
+++ packages/victory-core/src/victory-util/immutable.js
@@ -26,13 +26,16 @@ export default {
 
   shallowToJS(x, whitelist) {
     return this.isIterable(x)
-      ? x.reduce((prev, curr, key) => {
-          if (whitelist && whitelist[key]) {
-            curr = this.shallowToJS(curr);
-          }
-          prev[key] = curr;
-          return prev;
-        }, this.isList(x) ? [] : {})
+      ? x.reduce(
+          (prev, curr, key) => {
+            if (whitelist && whitelist[key]) {
+              curr = this.shallowToJS(curr);
+            }
+            prev[key] = curr;
+            return prev;
+          },
+          this.isList(x) ? [] : {}
+        )
       : x;
   }
 };
