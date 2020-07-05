89d8d40fda4356a7c4a7d10575471c1724e23e6d
boygirl <lauren.eastridge@formidable.com>
2020-07-05 16:01:06 -0700

    set up formideploy

---
+++ docs/formideploy.config.js
@@ -0,0 +1,13 @@
+/**
+ * Formideploy configuration overrides.
+ */
+module.exports = {
+  lander: {
+    name: "victory"
+  },
+  production: {
+    // TODO: REMOVE
+    domain: "tmp-experiment-02.formidable.com",
+    bucket: "tmp-experiment-02.formidable.com"
+  }
+};
