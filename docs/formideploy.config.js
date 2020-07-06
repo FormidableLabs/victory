cb22348b19300ee5e577675a17f32d750f45aa3a
boygirl <lauren.eastridge@formidable.com>
2020-07-06 16:33:17 -0700

    remove temporary branch names and prod domains

--- docs/formideploy.config.js
+++ docs/formideploy.config.js
@@ -4,10 +4,5 @@
 module.exports = {
   lander: {
     name: "victory"
-  },
-  production: {
-    // TODO: REMOVE
-    domain: "tmp-experiment-02.formidable.com",
-    bucket: "tmp-experiment-02.formidable.com"
   }
 };
