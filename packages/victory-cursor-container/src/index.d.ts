a01a240a58b30a313f02643386d90afae49ef827
boygirl <lauren.eastridge@formidable.com>
2021-05-07 12:43:27 -0700

    fix cursor coordinate prop

--- packages/victory-cursor-container/src/index.d.ts
+++ packages/victory-cursor-container/src/index.d.ts
@@ -1,15 +1,17 @@
 import * as React from "react";
 import { VictoryContainerProps, CoordinatesPropType, CallbackArgs } from "victory-core";
 
+export type CursorCoordinatesPropType = CoordinatesPropType | number;
+
 export interface VictoryCursorContainerProps extends VictoryContainerProps {
   cursorComponent?: React.ReactElement;
   cursorDimension?: "x" | "y";
-  cursorLabel?: (point: CoordinatesPropType, args: CallbackArgs) => any | void;
+  cursorLabel?: (point: CursorCoordinatesPropType, args: CallbackArgs) => any | void;
   cursorLabelComponent?: React.ReactElement;
-  cursorLabelOffset?: number | CoordinatesPropType;
-  defaultCursorValue?: number | CoordinatesPropType;
+  cursorLabelOffset?: CursorCoordinatesPropType;
+  defaultCursorValue?: CursorCoordinatesPropType;
   disable?: boolean;
-  onCursorChange?: (value: CoordinatesPropType, props: VictoryCursorContainerProps) => void;
+  onCursorChange?: (value: CursorCoordinatesPropType, props: VictoryCursorContainerProps) => void;
 }
 
 export class VictoryCursorContainer extends React.Component<VictoryCursorContainerProps, any> {}
