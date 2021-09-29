d44b99a51c99de38958f92510ea623ab3129f5d4
Grant Sander <gksander93@gmail.com>
2021-09-29 15:21:01 -0500

    Dedup react instances

--- demo/rn-js/metro.config.js
+++ demo/rn-js/metro.config.js
@@ -1,15 +1,31 @@
 const path = require("path");
-const { getDefaultConfig } = require('@expo/metro-config');
+const { getDefaultConfig } = require("@expo/metro-config");
+const blacklist = require("metro-config/src/defaults/blacklist");
+const escape = require("escape-string-regexp");
+
+const root = path.resolve(__dirname, "../..");
 
 const defaultConfig = getDefaultConfig(__dirname);
 
-defaultConfig.watchFolders = [path.resolve(__dirname, "../..")];
+defaultConfig.watchFolders = [root];
 
 defaultConfig.transformer.getTransformOptions = async () => ({
   transform: {
     experimentalImportSupport: false,
-    inlineRequires: true,
-  },
+    inlineRequires: true
+  }
 });
+
+// Ensuring we only use one version of React
+const modules = ["react"];
+defaultConfig.resolver.blacklistRE = blacklist(
+  modules.map(
+    (m) => new RegExp(`^${escape(path.join(root, "node_modules", m))}\\/.*$`)
+  )
+);
+defaultConfig.resolver.extraNodeModules = modules.reduce((acc, name) => {
+  acc[name] = path.join(__dirname, "node_modules", name);
+  return acc;
+}, {});
 
 module.exports = defaultConfig;
