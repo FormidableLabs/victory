f79758642c302d252552dfac5877c65f8188a23e
Becca Bailey <becca.bailey@formidable.com>
2021-09-28 18:58:07 -0700

    Update container to work with parent styles

--- stories/canvas-curve.stories.js
+++ stories/canvas-curve.stories.js
@@ -1,6 +1,16 @@
 import React from "react";
+import { CanvasContainer, Curve } from "../packages/victory-canvas/src";
+import { VictoryChart } from "../packages/victory-chart/src/index";
+import { VictoryTheme } from "../packages/victory-core/src/index";
 import { VictoryLine } from "../packages/victory-line/src";
-import { Curve, Container } from "../packages/victory-canvas/src";
+
+const containerStyle = {
+  display: "flex",
+  flexDirection: "row",
+  flexWrap: "wrap",
+  alignItems: "center",
+  justifyContent: "center"
+};
 
 const parentStyle = {
   parent: { border: "1px solid #ccc", margin: "2%", maxWidth: "40%" }
@@ -8,11 +18,32 @@ const parentStyle = {
 
 export const DefaultRendering = () => {
   return (
-    <VictoryLine
-      style={parentStyle}
-      dataComponent={<Curve />}
-      containerComponent={<Container />}
-    />
+    <div style={containerStyle}>
+      <VictoryLine
+        style={parentStyle}
+        dataComponent={<Curve />}
+        containerComponent={<CanvasContainer />}
+      />
+      <VictoryChart
+        style={parentStyle}
+        containerComponent={<CanvasContainer />}
+      >
+        <VictoryLine dataComponent={<Curve />} />
+      </VictoryChart>
+      <VictoryLine
+        style={parentStyle}
+        dataComponent={<Curve />}
+        containerComponent={<CanvasContainer />}
+        theme={VictoryTheme.material}
+      />
+      <VictoryChart
+        style={parentStyle}
+        containerComponent={<CanvasContainer />}
+        theme={VictoryTheme.material}
+      >
+        <VictoryLine dataComponent={<Curve />} />
+      </VictoryChart>
+    </div>
   );
 };
 
