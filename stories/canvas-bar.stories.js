91b061600755a4a068fcd5fad144bda9d07a8433
Becca Bailey <becca.bailey@formidable.com>
2021-10-06 09:44:17 -0700

    Re-add corner radius stories

--- stories/canvas-bar.stories.js
+++ stories/canvas-bar.stories.js
@@ -426,6 +426,102 @@ export const CornerRadius = () => {
           cornerRadius={7}
         />
       </VictoryChart>
+      <VictoryChart horizontal {...defaultChartProps}>
+        <VictoryBar
+          groupComponent={<CanvasGroup />}
+          dataComponent={<Bar />}
+          data={getMixedData(7)}
+          barWidth={40}
+          cornerRadius={20}
+        />
+      </VictoryChart>
+      <VictoryChart {...defaultChartProps}>
+        <VictoryBar
+          groupComponent={<CanvasGroup />}
+          dataComponent={<Bar />}
+          data={getMixedData(7)}
+          barWidth={40}
+          cornerRadius={20}
+        />
+      </VictoryChart>
+      <VictoryChart horizontal {...defaultChartProps}>
+        <VictoryBar
+          groupComponent={<CanvasGroup />}
+          dataComponent={<Bar />}
+          data={getMixedData(7)}
+          barWidth={40}
+          cornerRadius={{
+            topLeft: 5,
+            topRight: 20,
+            bottomLeft: 20,
+            bottomRight: 0
+          }}
+        />
+      </VictoryChart>
+      <VictoryChart {...defaultChartProps}>
+        <VictoryBar
+          groupComponent={<CanvasGroup />}
+          dataComponent={<Bar />}
+          data={getMixedData(7)}
+          barWidth={40}
+          cornerRadius={{
+            topLeft: 5,
+            topRight: 20,
+            bottomLeft: 20,
+            bottomRight: 0
+          }}
+        />
+      </VictoryChart>
+      <VictoryChart polar innerRadius={60} {...defaultChartProps}>
+        <VictoryBar
+          groupComponent={<CanvasGroup />}
+          dataComponent={<Bar />}
+          data={[
+            { x: 45, y: 6 },
+            { x: 90, y: 30 },
+            { x: 135, y: 65 },
+            { x: 180, y: 50 },
+            { x: 270, y: 40 },
+            { x: 315, y: 30 }
+          ]}
+          style={{ data: { fill: "tomato", width: 40 } }}
+          cornerRadius={{
+            topRight: 1,
+            topLeft: 20,
+            bottomRight: 5,
+            bottomLeft: 0
+          }}
+        />
+        <VictoryPolarAxis
+          labelPlacement="parallel"
+          tickValues={[0, 45, 90, 135, 180, 225, 270, 315]}
+        />
+      </VictoryChart>
+      <VictoryChart polar {...defaultChartProps}>
+        <VictoryBar
+          groupComponent={<CanvasGroup />}
+          dataComponent={<Bar />}
+          data={[
+            { x: 45, y: 6 },
+            { x: 90, y: 30 },
+            { x: 135, y: 65 },
+            { x: 180, y: 50 },
+            { x: 270, y: 40 },
+            { x: 315, y: 30 }
+          ]}
+          style={{ data: { fill: "tomato", width: 40 } }}
+          cornerRadius={{
+            topRight: 1,
+            topLeft: 20,
+            bottomRight: 5,
+            bottomLeft: 0
+          }}
+        />
+        <VictoryPolarAxis
+          labelPlacement="parallel"
+          tickValues={[0, 45, 90, 135, 180, 225, 270, 315]}
+        />
+      </VictoryChart>
     </div>
   );
 };
