5dd7ded80e29ed85059170372232a7759130a0b7
Grant Sander <gksander93@gmail.com>
2021-11-01 10:02:24 -0500

    Some things are working...

--- packages/victory-native/jest.config.js
+++ packages/victory-native/jest.config.js
@@ -2,6 +2,6 @@ module.exports = {
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
   testMatch: ["**/?(*.)+(spec|test).[jt]s?(x)"],
   transform: {
-    "^.+\\.(js|jsx)$": "babel-jest"
+    "^.+\\.(js|jsx)$": ["babel-jest", { configFile: "./babel.config.js" }]
   }
 };
