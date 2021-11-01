55794c8807837073b536189079161f3df7918a1c
Grant Sander <gksander93@gmail.com>
2021-11-01 14:17:44 -0500

    Jest into root, get native-helpers test running

---
+++ jest.native.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  testMatch: ["**/jest/**/?(*.)+(spec|test).[jt]s?(x)"],
+  transform: {
+    "^.+\\.(js|jsx)$": [
+      "babel-jest",
+      { configFile: "./babel.native.config.js" }
+    ]
+  }
+};
--- packages/victory-native/jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-  testMatch: ["**/?(*.)+(spec|test).[jt]s?(x)"],
-  transform: {
-    "^.+\\.(js|jsx)$": ["babel-jest", { configFile: "./babel.config.js" }]
-  }
-};
