c0061e8f91b4f686c787362acef2af66c15156e3
Becca Bailey <becca.bailey@formidable.com>
2022-03-14 11:21:18 -0700

    Upgrade storybook to v6

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,47 +9,40 @@ module.exports = {
       }
     }
   },
-  extends: [
-    "formidable/configurations/es6-react",
-    "prettier",
-    "plugin:react-hooks/recommended"
-  ],
+  extends: ["formidable/configurations/es6-react", "prettier", "plugin:react-hooks/recommended", "plugin:storybook/recommended"],
   rules: {
     "react/sort-comp": "off",
-    "import/no-unresolved": [2, { ignore: ["victory*"] }],
+    "import/no-unresolved": [2, {
+      ignore: ["victory*"]
+    }],
     "max-statements": 0,
-    complexity: ["error", { max: 16 }],
-    "no-magic-numbers": [
-      "error",
-      { ignore: [-1, 0, 0.5, 1, 2, 90, 180, 270, 360] }
-    ]
+    complexity: ["error", {
+      max: 16
+    }],
+    "no-magic-numbers": ["error", {
+      ignore: [-1, 0, 0.5, 1, 2, 90, 180, 270, 360]
+    }]
   },
   plugins: ["jest"],
   env: {
     "jest/globals": true
   },
-  overrides: [
-    {
-      files: ["*.ts", "*.tsx"],
-      extends: [
-        "plugin:@typescript-eslint/eslint-recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:@typescript-eslint/recommended-requiring-type-checking"
-      ],
-      parser: "@typescript-eslint/parser",
-      parserOptions: {
-        project: "./tsconfig.json",
-        tsconfigRootDir: __dirname
-      },
-      plugins: ["@typescript-eslint"],
-      rules: {
-        "@typescript-eslint/no-explicit-any": "off",
-        "@typescript-eslint/explicit-function-return-type": "off",
-        "no-magic-numbers": "off",
-        "import/no-duplicates": "off",
-        "react/no-multi-comp": "off",
-        "react/sort-comp": "off"
-      }
+  overrides: [{
+    files: ["*.ts", "*.tsx"],
+    extends: ["plugin:@typescript-eslint/eslint-recommended", "plugin:@typescript-eslint/recommended", "plugin:@typescript-eslint/recommended-requiring-type-checking"],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+      project: "./tsconfig.json",
+      tsconfigRootDir: __dirname
+    },
+    plugins: ["@typescript-eslint"],
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/explicit-function-return-type": "off",
+      "no-magic-numbers": "off",
+      "import/no-duplicates": "off",
+      "react/no-multi-comp": "off",
+      "react/sort-comp": "off"
     }
-  ]
+  }]
 };
