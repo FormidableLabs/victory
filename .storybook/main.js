ce2b065546a2922ed1310501e27b2e4e909aa37a
Becca Bailey <becca.bailey@formidable.com>
2022-03-14 12:49:32 -0700

    Run format script

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,21 +1,25 @@
 var path = require("path");
 
 module.exports = {
-  addons: ["storybook-addon-performance/register", "@storybook/addon-options/register", {
-    name: "@storybook/addon-storysource",
-    options: {
-      rule: {
-        test: [/\.stories\.jsx?$/],
-        include: [path.resolve(__dirname, "../stories")]
-      },
-      loaderOptions: {
-        prettierConfig: {
-          printWidth: 80,
-          singleQuote: false
+  addons: [
+    "storybook-addon-performance/register",
+    "@storybook/addon-options/register",
+    {
+      name: "@storybook/addon-storysource",
+      options: {
+        rule: {
+          test: [/\.stories\.jsx?$/],
+          include: [path.resolve(__dirname, "../stories")]
+        },
+        loaderOptions: {
+          prettierConfig: {
+            printWidth: 80,
+            singleQuote: false
+          }
         }
       }
     }
-  }],
+  ],
   stories: ["../stories/*.stories.js"],
   core: {
     builder: "webpack5"
