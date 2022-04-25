2ba67c0864bf774ca1f24aba8741cad20cfca3dc
Ryan Roemer <ryan.roemer@formidable.com>
2022-04-25 13:27:53 -0700

    WIP: Add vendor build process for all libraries

--- packages/victory-vendor/lib/d3-interpolate.js
+++
@@ -1,3 +0,0 @@
-
-// TODO: Need to expand to separate files or something?
-module.exports = require("../lib-vendor/d3-interpolate/src/index.js");
--- packages/victory-vendor/scripts/build.js
+++ packages/victory-vendor/scripts/build.js
@@ -3,23 +3,24 @@
  */
 const fs = require("fs/promises");
 const path = require("path");
-const PKGS = [
-  "d3-interpolate"
-];
+const { promisify } = require("util");
+
+const rimraf = require("rimraf");
+const rimrafP = promisify(rimraf);
 
 // Templates.
-const getIndexEsm = (pkg) => `
+const getEsmIndex = (pkg) => `
 // \`victory-vendor/${pkg.name}\` (ESM)
 // See upstream license: ${pkg.repository.url.replace(/\.git$/, "")}/blob/main/LICENSE
 //
 // Our ESM package uses the underlying installed dependencies of \`node_modules/${pkg.name}\`
-export * from "d3-interpolate";
+export * from "${pkg.name}";
 
 // TODO REMOVE
 console.log("TODO LOADED ESM ${pkg.name}");
 `;
 
-const getIndexCjs = (pkg) => `
+const getCjsIndex = (pkg) => `
 // \`victory-vendor/${pkg.name}\` (CommonJS)
 // See upstream license: ${pkg.repository.url.replace(/\.git$/, "")}/blob/main/LICENSE
 //
@@ -32,23 +33,85 @@ console.log("TODO LOADED CJS ${pkg.name}");
 
 // Main.
 const main = async () => {
-  Promise.all(PKGS.map(async (pkgName) => {
-    console.log("TODO HERE", pkgName);
+  // Lazy ESM imports.
+  const { execa } = await import("execa");
 
-    const pkgPath = path.resolve(__dirname, `../node_modules/${pkgName}/package.json`);
+  // Get d3-related packages we want to vendor.
+  const pkgs = (await fs.readdir(path.resolve(__dirname, "../node_modules/")))
+    .filter((name) => /^(d3-|internmap)/.test(name));
+
+  // Safety check: we assume that **all** are flattened to root level of this
+  // package, and want to make sure there are no nested dependencies.
+  for (let pkgName of pkgs) {
+    const pkgModsPath = path.resolve(__dirname, `../node_modules/git${pkgName}/node_modules`);
+    const stat = await fs.lstat(pkgModsPath).catch(() => null);
+    if (stat) {
+      throw new Error(`Found nested modules: ${pkgModsPath}`);
+    }
+  }
+
+  // Clean out and ensure base library paths exist
+  const EsmBasePath = path.resolve(__dirname, `../es`);
+  const CjsBasePath = path.resolve(__dirname, `../lib`);
+  const VendorBasePath = path.resolve(__dirname, `../lib-vendor`);
+  console.log("Cleaning old vendor directories.");
+  await Promise.all(
+    [
+      EsmBasePath,
+      CjsBasePath,
+      VendorBasePath
+    ]
+    .map((libPath) => rimrafP(libPath)
+      .then(() => fs.mkdir(libPath, { recursive: true }))
+    )
+  );
+
+  // Transpile.
+  console.log("Transpiling vendor sources.");
+  await execa("yarn" /*yarn*/, [
+    "babel",
+    "--config-file", path.resolve(__dirname, "../.babelrc.js"),
+    "-d", path.resolve(__dirname, "../lib-vendor"),
+    path.resolve(__dirname, "../node_modules")
+  ], {
+    stdio: "inherit"
+  });
+
+  // Iterate and generate index files.
+  console.log("Copying licenses and generating indexes.");
+  for (let pkgName of pkgs) {
+    console.log(`- ${pkgName}`);
+
+    const pkgBase = path.resolve(__dirname, `../node_modules/${pkgName}`);
+    const pkgPath = path.join(pkgBase, `package.json`);
     const pkg = await fs.readFile(pkgPath).then((buf) => JSON.parse(buf.toString()));
-    console.log(getIndexEsm(pkg));
-    console.log(getIndexCjs(pkg));
-  }));
+    const libVendorPath = path.resolve(__dirname, `../lib-vendor/${pkgName}`);
+
+    // Create library indexes and copy licenses to `lib-vendor.
+    await Promise.all([
+      fs.writeFile(
+        path.join(EsmBasePath, `${pkgName}.js`),
+        getEsmIndex(pkg)
+      ),
+      fs.writeFile(
+        path.join(CjsBasePath, `${pkgName}.js`),
+        getCjsIndex(pkg)
+      ),
+      fs.copyFile(
+        path.join(pkgBase, "LICENSE"),
+        path.join(libVendorPath, "LICENSE")
+      )
+    ]);
+  }
 }
 
 if (require.main === module) {
   main()
-  .then(() => {
-    console.log("Build finished.")
-  })
-  .catch((err) => {
-    console.error(err);
-    process.exit(-1);
-  })
+    .then(() => {
+      console.log("Build finished.")
+    })
+    .catch((err) => {
+      console.error(err);
+      process.exit(-1);
+    });
 }
