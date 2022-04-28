919df4589fd81381710fd30e5fcdbc35ed8ea4a8
Ryan Roemer <ryan.roemer@formidable.com>
2022-04-28 11:17:07 -0700

    Remove unused karma stuff

--- config/karma/karma.conf.js
+++ config/karma/karma.conf.js
@@ -6,18 +6,13 @@
  * the test files one-off for just a single run. This is appropriate for a
  * CI environment or if you're not otherwise running `npm run dev|hot`.
  */
-var path = require("path");
-var webpack = require("webpack");
-var webpackCfg = require("../webpack/webpack.config.test");
+const path = require("path");
+const webpack = require("webpack");
+const webpackCfg = require("../webpack/webpack.config.test");
 
-var MAIN_PATH = path.join(process.cwd(), "test/client/main.js");
-var POLYFILL_PATH = path.join(
-  path.dirname(require.resolve("core-js/package.json")),
-  "es6/**/*.js"
-);
-var PREPROCESSORS = {};
+const MAIN_PATH = path.resolve("test/client/main.js");
+const PREPROCESSORS = {};
 PREPROCESSORS[MAIN_PATH] = ["webpack"];
-PREPROCESSORS[POLYFILL_PATH] = ["webpack"];
 
 module.exports = function (config) {
   /* eslint-disable global-require */
@@ -32,10 +27,6 @@ module.exports = function (config) {
     files: [
       // Sinon has issues with webpack. Do global include.
       require.resolve("sinon/pkg/sinon"),
-
-      // Polyfills for PhantomJS in React 16.
-      require.resolve("core-js/es6/map"),
-      require.resolve("core-js/es6/set"),
 
       // Test bundle (created via local webpack-dev-server in this config).
       MAIN_PATH
