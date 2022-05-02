02145e216ae9fbfa338e8b9947dd36ab9a78ae90
Becca Bailey <becca.bailey@formidable.com>
2022-05-02 15:41:59 -0700

    Update native config

--- jest-config.js
+++ jest-config.js
@@ -2,7 +2,11 @@ module.exports = {
   testEnvironment: "jsdom",
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
   testMatch: ["**/jest/**/?(*.)+(spec|test).[jt]s?(x)"],
+  testPathIgnorePatterns: ["victory-native"],
   transform: {
-    "^.+\\.(js|jsx)$": ["babel-jest", { configFile: "./babel-config.js" }]
+    "^.+\\.(js|jsx)$": [
+      "babel-jest",
+      { configFile: "./babel-native-config.js" }
+    ]
   }
 };
--- jest-native-config.js
+++ jest-native-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-  testMatch: ["**/jest/**/?(*.)+(spec|test).[jt]s?(x)"],
+  testMatch: ["**/jest/victory-native/**/?(*.)+(spec|test).[jt]s?(x)"],
   transform: {
     "^.+\\.(js|jsx)$": [
       "babel-jest",
