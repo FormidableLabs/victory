cd0496f49403a323d686f2025b06a8878fac96d9
scottrippey <scott.william.rippey@gmail.com>
2022-05-24 13:35:36 -0600

    test(jest): updated log to improve stack traces

--- packages/victory-core/src/victory-util/log.js
+++ packages/victory-core/src/victory-util/log.js
@@ -2,11 +2,7 @@
 /* eslint-disable no-console */
 
 // TODO: Use "warning" npm module like React is switching to.
-// eslint-disable-next-line func-style
-export function warn(message) {
-  if (process.env.NODE_ENV !== "production") {
-    if (console && console.warn) {
-      console.warn(message);
-    }
-  }
-}
+export const warn =
+  process.env.NODE_ENV !== "production"
+    ? console.warn
+    : function ignoreWarning() {};
