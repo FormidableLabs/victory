bc8c898343a7631bf2de5fe568bccd91d6d857dd
scottrippey <scott.william.rippey@gmail.com>
2022-05-31 14:30:09 -0600

    chore(typescript): migrated `victory-label` to typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -44,9 +44,19 @@ module.exports = {
         "plugin:@typescript-eslint/recommended-requiring-type-checking"
       ],
       rules: {
-        "@typescript-eslint/no-unsafe-assignment": "off",
-        "@typescript-eslint/no-unsafe-call": "off",
-        "@typescript-eslint/no-unsafe-member-access": "off"
+        "no-use-before-define": "off",
+        "@typescript-eslint/no-use-before-define": [
+          "error",
+          { typedefs: false }
+        ],
+
+        "@typescript-eslint/no-unsafe-argument": "warn",
+        "@typescript-eslint/no-unsafe-assignment": "warn",
+        "@typescript-eslint/no-unsafe-return": "warn",
+        "@typescript-eslint/no-unsafe-call": "warn",
+        "@typescript-eslint/no-unsafe-member-access": "warn",
+        "@typescript-eslint/restrict-template-expressions": "warn",
+        "@typescript-eslint/restrict-plus-operands": "warn"
       }
     }
   ]
