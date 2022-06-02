83b8e3c2933fa4cb6959e1c8388aed0233f9fa19
scottrippey <scott.william.rippey@gmail.com>
2022-06-02 15:54:15 -0600

    chore(typescript): fixed formatting issue

--- packages/victory-core/src/victory-primitives/tspan.tsx
+++ packages/victory-core/src/victory-primitives/tspan.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { VictoryCommonPrimitiveProps } from "../victory-util/types";
 
-// @ts-expect-error FIXME: "id cannot be a number"
 export const TSpan = (props: VictoryCommonPrimitiveProps) => (
+  // @ts-expect-error FIXME: "id cannot be a number"
   <tspan {...props} />
 );
