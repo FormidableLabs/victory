801f5e2ed1222338d62d65ea0daaf71701106c20
scottrippey <scott.william.rippey@gmail.com>
2022-06-09 12:21:52 -0600

    chore(typescript): migrate `immutable`

--- packages/victory-core/src/victory-util/immutable.ts
+++ packages/victory-core/src/victory-util/immutable.ts
@@ -1,37 +1,41 @@
+import type { Iterable, Record, List, Map } from "immutable";
+
 export const IMMUTABLE_ITERABLE = "@@__IMMUTABLE_ITERABLE__@@";
 export const IMMUTABLE_RECORD = "@@__IMMUTABLE_RECORD__@@";
 export const IMMUTABLE_LIST = "@@__IMMUTABLE_LIST__@@";
 export const IMMUTABLE_MAP = "@@__IMMUTABLE_MAP__@@";
 
-export function isIterable(x) {
+export function isIterable(x): x is Iterable<unknown, unknown> {
   return !!(x && x[IMMUTABLE_ITERABLE]);
 }
 
-export function isRecord(x) {
+export function isRecord(x): x is Record<string, unknown> {
   return !!(x && x[IMMUTABLE_RECORD]);
 }
 
-export function isImmutable(x) {
+export function isImmutable(
+  x
+): x is Iterable<unknown, unknown> | Record<string, unknown> {
   return isIterable(x) || isRecord(x);
 }
 
-export function isList(x) {
+export function isList(x): x is List<unknown> {
   return !!(x && x[IMMUTABLE_LIST]);
 }
 
-export function isMap(x) {
+export function isMap(x): x is Map<unknown, unknown> {
   return !!(x && x[IMMUTABLE_MAP]);
 }
 
-export function shallowToJS(x, whitelist) {
+export function shallowToJS(x, whitelist?: Record<string, boolean | unknown>) {
   return isIterable(x)
     ? x.reduce(
-        (prev, curr, key) => {
+        (result: any, curr: any, key: any) => {
           if (whitelist && whitelist[key]) {
             curr = shallowToJS(curr);
           }
-          prev[key] = curr;
-          return prev;
+          result[key] = curr;
+          return result;
         },
         isList(x) ? [] : {}
       )
