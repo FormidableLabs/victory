16d51aa99e879fc723a414fb1f155105d9622cbd
Becca Bailey <becca.bailey@formidable.com>
2022-06-10 14:21:23 -0700

    Add initial beta file for victory-bar

---
+++ packages/victory-bar/src/beta.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+import { VictoryProviderProps } from "victory-core";
+
+interface VictoryBarProps extends VictoryProviderProps {}
+
+export function VictoryBar(props: VictoryBarProps) {}
