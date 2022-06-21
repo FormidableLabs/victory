6c312c308478b06cceba0789a832e7adf96ca273
scottrippey <scott.william.rippey@gmail.com>
2022-06-20 20:57:07 -0600

    chore(tests): fix test types

--- test/jest/victory-area/victory-area.test.tsx
+++ test/jest/victory-area/victory-area.test.tsx
@@ -251,7 +251,7 @@ describe("components/victory-area", () => {
           data={ariaTestData}
           dataComponent={
             <Area
-              ariaLabel={({ data }) => `data point 1's x value is ${data[0].x}`}
+              ariaLabel={({ data }) => `data point 1's x value is ${data![0].x}`}
               tabIndex={4}
             />
           }
