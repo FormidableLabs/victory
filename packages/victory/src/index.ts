a92d54f5a0327c20431350c048af703e9361e4ed
scottrippey <scott.william.rippey@gmail.com>
2022-06-27 10:38:21 -0600

    chore(typescript): ensure all types are exported too

--- packages/victory/src/index.ts
+++ packages/victory/src/index.ts
@@ -1,11 +1,17 @@
 export {
   addEvents,
+  Arc,
+  type ArcProps,
   Axis,
   Background,
+  type BackgroundProps,
   Border,
+  type BorderProps,
   Box,
+  type BoxProps,
   Circle,
   ClipPath,
+  type ClipPathProps,
   Collection,
   Data,
   DefaultTransitions,
@@ -15,48 +21,105 @@ export {
   LabelHelpers,
   Line,
   LineSegment,
+  type LineSegmentProps,
   Log,
   Path,
   Point,
+  type PointProps,
   Portal,
   PropTypes,
   Rect,
   Scale,
   Selection,
   Style,
+  TSpan,
   Text,
+  type TextProps,
   TextSize,
   Transitions,
-  TSpan,
   UserProps,
   VictoryAccessibleGroup,
+  type VictoryAccessibleGroupProps,
   VictoryAnimation,
+  type VictoryAnimationProps,
   VictoryClipContainer,
+  type VictoryClipContainerProps,
   VictoryContainer,
+  type VictoryContainerProps,
   VictoryLabel,
+  type VictoryLabelProps,
   VictoryPortal,
+  type VictoryPortalProps,
   VictoryTheme,
+  type VictoryThemeDefinition,
   VictoryTransition,
   Whisker,
+  type WhiskerProps,
   Wrapper,
 } from "victory-core";
 
-export { VictoryChart } from "victory-chart";
-export { VictoryGroup } from "victory-group";
-export { VictoryStack } from "victory-stack";
-export { VictoryPie, Slice } from "victory-pie";
-export { VictoryArea, Area } from "victory-area";
-export { VictoryBar, Bar } from "victory-bar";
-export { VictoryCandlestick, Candle } from "victory-candlestick";
-export { VictoryErrorBar, ErrorBar } from "victory-errorbar";
-export { VictoryHistogram } from "victory-histogram";
-export { VictoryLine, Curve } from "victory-line";
-export { VictoryScatter } from "victory-scatter";
-export { VictoryBoxPlot } from "victory-box-plot";
-export { VictoryVoronoi, Voronoi } from "victory-voronoi";
-export { VictoryBrushLine } from "victory-brush-line";
+export { VictoryChart, type VictoryChartProps } from "victory-chart";
+export { VictoryGroup, type VictoryGroupProps } from "victory-group";
+export { VictoryStack, type VictoryStackProps } from "victory-stack";
+export {
+  VictoryPie,
+  Slice,
+  type VictoryPieProps,
+  type SliceProps,
+} from "victory-pie";
+export {
+  VictoryArea,
+  Area,
+  type VictoryAreaProps,
+  type AreaProps,
+} from "victory-area";
+export {
+  VictoryBar,
+  Bar,
+  type VictoryBarProps,
+  type BarProps,
+} from "victory-bar";
+
+export {
+  VictoryCandlestick,
+  type VictoryCandlestickProps,
+  Candle,
+  type CandleProps,
+} from "victory-candlestick";
+export {
+  VictoryErrorBar,
+  ErrorBar,
+  type VictoryErrorBarProps,
+  type ErrorBarProps,
+} from "victory-errorbar";
+
+export {
+  VictoryHistogram,
+  type VictoryHistogramProps,
+} from "victory-histogram";
+export {
+  VictoryLine,
+  Curve,
+  type VictoryLineProps,
+  type CurveProps,
+} from "victory-line";
+
+export { VictoryScatter, type VictoryScatterProps } from "victory-scatter";
+export { VictoryBoxPlot, type VictoryBoxPlotProps } from "victory-box-plot";
+export {
+  Voronoi,
+  VictoryVoronoi,
+  type VoronoiProps,
+  type VictoryVoronoiProps,
+} from "victory-voronoi";
+
+export {
+  VictoryBrushLine,
+  type VictoryBrushLineProps,
+} from "victory-brush-line";
 export {
   VictoryBrushContainer,
+  type VictoryBrushContainerProps,
   // @ts-expect-error Missing types for this export
   BrushHelpers,
   // @ts-expect-error Missing types for this export
@@ -64,6 +127,7 @@ export {
 } from "victory-brush-container";
 export {
   VictoryCursorContainer,
+  type VictoryCursorContainerProps,
   // @ts-expect-error Missing types for this export
   CursorHelpers,
   // @ts-expect-error Missing types for this export
@@ -71,6 +135,7 @@ export {
 } from "victory-cursor-container";
 export {
   VictorySelectionContainer,
+  type VictorySelectionContainerProps,
   // @ts-expect-error Missing types for this export
   SelectionHelpers,
   // @ts-expect-error Missing types for this export
@@ -78,6 +143,7 @@ export {
 } from "victory-selection-container";
 export {
   VictoryVoronoiContainer,
+  type VictoryVoronoiContainerProps,
   // @ts-expect-error Missing types for this export
   VoronoiHelpers,
   // @ts-expect-error Missing types for this export
@@ -85,6 +151,7 @@ export {
 } from "victory-voronoi-container";
 export {
   VictoryZoomContainer,
+  type VictoryZoomContainerProps,
   // @ts-expect-error Missing types for this export
   ZoomHelpers,
   // @ts-expect-error Missing types for this export
@@ -98,18 +165,35 @@ export {
   // @ts-expect-error Missing types for this export
   makeCreateContainerFunction,
   createContainer,
+  type ContainerType,
 } from "victory-create-container";
 
-export { VictoryTooltip, Flyout } from "victory-tooltip";
-export { VictoryLegend } from "victory-legend";
-export { VictorySharedEvents } from "victory-shared-events";
-export { VictoryAxis } from "victory-axis";
-export { VictoryPolarAxis } from "victory-polar-axis";
+export {
+  VictoryTooltip,
+  Flyout,
+  type VictoryTooltipProps,
+  type FlyoutProps,
+} from "victory-tooltip";
+export { VictoryLegend, type VictoryLegendProps } from "victory-legend";
+export {
+  VictorySharedEvents,
+  type VictorySharedEventsProps,
+} from "victory-shared-events";
+export { VictoryAxis, type VictoryAxisProps } from "victory-axis";
+export {
+  VictoryPolarAxis,
+  type VictoryPolarAxisProps,
+} from "victory-polar-axis";
+
 export {
   CanvasGroup,
+  type CanvasGroupProps,
   CanvasCurve,
+  type CanvasCurveProps,
   CanvasPoint,
+  type CanvasPointProps,
   CanvasBar,
+  type CanvasBarProps,
   // @ts-expect-error Missing types for this export
   useCanvasContext,
 } from "victory-canvas";
