342d595460b4e8a56011540b42ce724385d1c3ab
Kheva Mann <khevamann@gmail.com>
2022-07-13 10:13:41 -0700

    Type export should not be with other exports
    This fixes an issue with types throwing an error https://github.com/FormidableLabs/victory/issues/2355

--- packages/victory-core/src/index.ts
+++ packages/victory-core/src/index.ts
@@ -12,9 +12,7 @@ export * from "./victory-portal/portal";
 export * from "./victory-portal/portal-context";
 export * from "./victory-portal/victory-portal";
 export * from "./victory-primitives";
-export {
-  Border as Box,
-  type BorderProps as BoxProps,
-} from "./victory-primitives";
+export { Border as Box } from './victory-primitives'
+export type { BorderProps as BoxProps } from './victory-primitives'
 export * from "./victory-util";
 export * from "./victory-state";
