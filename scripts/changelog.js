c7ff5168ee5d13984023febeda2074a30b4b9fd5
grose <gwyneth.rose@formidable.com>
2022-12-07 17:06:33 -0700

    Fixed prettier errors in scripts/changelog.js

--- scripts/changelog.js
+++ scripts/changelog.js
@@ -1,13 +1,13 @@
-import { getReleaseLine } from '@svitejs/changesets-changelog-github-compact';
+import { getReleaseLine } from "@svitejs/changesets-changelog-github-compact";
 
 const changelogFunctions = {
   // Dependencies are updated on every changeset for every package, so
   // remove them to reduce noise.
-  getDependencyReleaseLine: () => '',
-  getReleaseLine
+  getDependencyReleaseLine: () => "",
+  getReleaseLine,
 };
 
 export default {
   ...changelogFunctions,
-  default: changelogFunctions
+  default: changelogFunctions,
 };
