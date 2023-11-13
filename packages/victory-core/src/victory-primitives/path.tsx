517f82b4b4017edb1c9e13b218256492139e3a91
Cesar Perez <cperez5@lululemon.com>
2023-11-13 09:08:54 -0700

    added ability to forward refs in Bar and Path components

--- packages/victory-core/src/victory-primitives/path.tsx
+++ packages/victory-core/src/victory-primitives/path.tsx
@@ -1,16 +1,20 @@
-import React from "react";
+import React, { forwardRef } from "react";
 import { VictoryPrimitiveShapeProps } from "./types";
 
-export const Path = (props: VictoryPrimitiveShapeProps) => {
+// eslint-disable-next-line prefer-arrow-callback
+export const Path = forwardRef(function Path(
+  props: VictoryPrimitiveShapeProps,
+  ref,
+) {
   // eslint-disable-next-line react/prop-types
   const { desc, ...rest } = props;
   return desc ? (
     // @ts-expect-error FIXME: "id cannot be a number"
-    <path {...rest}>
+    <path {...rest} ref={ref}>
       <desc>{desc}</desc>
     </path>
   ) : (
     // @ts-expect-error FIXME: "id cannot be a number"
-    <path {...rest} />
+    <path {...rest} ref={ref} />
   );
-};
+});
