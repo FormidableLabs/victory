ebe705156382b4b077d562e2d002b4957815223b
Kenan <kenan.m.yusuf@gmail.com>
2024-01-10 09:51:33 +0000

    Migrate victory-selection-container to TS

--- packages/victory-selection-container/src/index.js
+++
@@ -1,5 +0,0 @@
-export {
-  selectionContainerMixin,
-  default as VictorySelectionContainer,
-} from "./victory-selection-container";
-export { default as SelectionHelpers } from "./selection-helpers";
