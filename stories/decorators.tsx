3c01eb013ef2c87cebedd00274e475b7a25c7383
Charlie Brown <carbonrobot@gmail.com>
2024-01-26 12:40:16 -0600

    Remove unused decorators

--- stories/decorators.tsx
+++ stories/decorators.tsx
@@ -1,26 +1,5 @@
 import React from "react";
 
-import { VictoryChart } from "../packages/victory-chart";
-import { VictoryPolarAxis } from "../packages/victory-polar-axis";
-import { VictoryTheme } from "../packages/victory-core";
-
-export const getChartDecorator = (props) => {
-  return (story) => {
-    return <VictoryChart {...props}>{story()}</VictoryChart>;
-  };
-};
-
-export const getPolarChartDecorator = (props) => {
-  return (story) => {
-    return (
-      <VictoryChart polar theme={VictoryTheme.material} {...props}>
-        {story()}
-        <VictoryPolarAxis />
-      </VictoryChart>
-    );
-  };
-};
-
 const containerStyle: React.CSSProperties = {
   display: "flex",
   flexDirection: "row",
