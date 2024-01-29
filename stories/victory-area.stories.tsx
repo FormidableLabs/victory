661c57bf2429651b9d7eac7da68435c3bc7025af
Kavitha <acharyakavita12@gmail.com>
2024-01-29 10:58:07 -0500

    pass newBaseProps in events.ts

--- stories/victory-area.stories.tsx
+++ stories/victory-area.stories.tsx
@@ -516,3 +516,37 @@ export const DisableInlineStyles = () => {
     </>
   );
 };
+
+export const events = () => {
+  return (
+    <div style={{ margin: 50 }}>
+      <h3>Click Me</h3>
+      <VictoryChart>
+        <VictoryArea
+          style={{
+            data: { fill: "#c43a31" }
+          }}
+          events={[{
+            target: "data",
+            eventHandlers: {
+              onClick: () => {
+                return [
+                  {
+                    eventKey: "all",
+                    target: "data",
+                    mutation: (props) => {
+                      const fill = props.style && props.style.fill;
+                      console.log(fill)
+                      return fill === "black" ? null : { style: { fill: "black" } };
+                    }
+                  }
+                ];
+              }
+            }
+          }]}
+          data={getData(5)}
+        />
+      </VictoryChart>
+    </div>
+  );
+};
