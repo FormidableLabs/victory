0a737fac3069ea352fb15e620dfba0b00ca315c1
Kenan <kenan.m.yusuf@gmail.com>
2024-02-06 10:32:14 +0000

    Fix wrapCoreComponent HOC types

--- packages/victory-native/src/components/victory-errorbar.tsx
+++ packages/victory-native/src/components/victory-errorbar.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { Dimensions } from "react-native";
 import { G } from "react-native-svg";
-import { VictoryErrorBar as VictoryErrorBarBase } from "victory-errorbar/es";
+import {
+  VictoryErrorBar as VictoryErrorBarBase,
+  VictoryErrorBarProps,
+} from "victory-errorbar/es";
 import { VictoryContainer } from "./victory-container";
 import { ErrorBar } from "./victory-primitives/error-bar";
 import { wrapCoreComponent } from "../helpers/wrap-core-component";
 
-export const VictoryErrorBar = wrapCoreComponent({
+export const VictoryErrorBar = wrapCoreComponent<VictoryErrorBarProps>({
   Component: VictoryErrorBarBase,
   defaultProps: {
     ...VictoryErrorBarBase.defaultProps,
