3da82636ddea203d87d3f27cf93c04eb73eef7f1
Kavitha <acharyakavita12@gmail.com>
2024-02-09 15:19:39 -0500

    add storybook test

--- stories/victory-pie.stories.tsx
+++ stories/victory-pie.stories.tsx
@@ -547,6 +547,24 @@ export const LabelPlacement = () => {
       <VictoryPie
         style={{ ...parentStyle, labels: { fill: "magenta" } }}
         radius={120}
+        labelPosition="startAngle"
+        labelPlacement="perpendicular"
+        labelComponent={<VictoryTooltip active />}
+        labels={({ datum }) => `${datum.l}\ndegrees`}
+        data={[
+          { x: 1, y: 1, l: 0 },
+          { x: 2, y: 1, l: 45 },
+          { x: 3, y: 1, l: 90 },
+          { x: 4, y: 1, l: 135 },
+          { x: 5, y: 1, l: 180 },
+          { x: 6, y: 1, l: 225 },
+          { x: 7, y: 1, l: 270 },
+          { x: 8, y: 1, l: 315 },
+        ]}
+      />
+      <VictoryPie
+        style={{ ...parentStyle, labels: { fill: "magenta" } }}
+        radius={120}
         labelPlacement={"curved"}
         labels={({ datum }) => `${datum.l} deg`}
         labelRadius={100}
