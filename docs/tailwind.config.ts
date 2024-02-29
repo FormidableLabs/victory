0dbf996c79527b7df55874a1abe68be71fb6d5a2
Charlie Brown <carbonrobot@gmail.com>
2024-02-29 15:49:26 -0600

    Fix formatting

--- docs/tailwind.config.ts
+++ docs/tailwind.config.ts
@@ -1,9 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx,mdx,md}",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx,md}"],
   theme: {
     extend: {
       backgroundImage: {
