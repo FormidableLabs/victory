872572882daf15de5e18e1811902d101567da6ae
Stef Williams <stef.williams@formidable.com>
2024-10-01 14:12:11 -0400

    feat: retheme victory-demo with clean theme

--- demo/ts/components/victory-demo.tsx
+++ demo/ts/components/victory-demo.tsx
@@ -36,80 +36,13 @@ export default class App extends React.Component {
           <VictoryLine y={(data) => data.x * data.x} />
         </VictoryChart>
 
+        <h3>VictoryChart</h3>
+        <p>Custom axes and tickformats; Bar + line chart</p>
         <VictoryChart
           theme={VictoryTheme.clean}
           style={style}
           domainPadding={{ x: 30, y: 30 }}
-          height={600}
-          width={600}
-          events={[
-            {
-              childName: "bar",
-              target: "data",
-              eventHandlers: {
-                onClick: () => {
-                  return [
-                    {
-                      target: "labels",
-                      mutation: () => {
-                        return { text: "o shit" };
-                      },
-                    },
-                    {
-                      childName: "line",
-                      eventKey: "all",
-                      target: "data",
-                      mutation: () => {
-                        return { style: { stroke: "lime" } };
-                      },
-                    },
-                    {
-                      childName: "line",
-                      eventKey: 0,
-                      target: "labels",
-                      mutation: () => {
-                        return {
-                          style: { fill: "green" },
-                          text: "waddup",
-                        };
-                      },
-                    },
-                  ];
-                },
-              },
-            },
-          ]}
         >
-          <VictoryBar
-            name="bar"
-            style={{ data: { width: 15, fill: "green" } }}
-            data={[
-              { x: 1, y: 1 },
-              { x: 2, y: 2 },
-              { x: 3, y: 3 },
-              { x: 4, y: 2 },
-              { x: 5, y: 1 },
-              { x: 6, y: 2 },
-              { x: 7, y: 3 },
-              { x: 8, y: 2 },
-              { x: 9, y: 1 },
-              { x: 10, y: 2 },
-              { x: 11, y: 3 },
-              { x: 12, y: 2 },
-              { x: 13, y: 1 },
-            ]}
-          />
-          <VictoryLine
-            name="line"
-            y={() => 0.5}
-            style={{ data: { stroke: "blue", strokeWidth: 5 } }}
-            labels={["LINE"]}
-          />
-        </VictoryChart>
-
-        <h3>VictoryChart</h3>
-        <p>Custom axes and tickformats; Bar + line chart</p>
-        <VictoryChart theme={VictoryTheme.clean} style={style} domainPadding={{ x: 30, y: 30 }}>
           <VictoryAxis
             tickValues={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]}
             tickFormat={(x) => `${x}\ntick`}
@@ -130,7 +63,11 @@ export default class App extends React.Component {
             }}
           />
           <VictoryBar
-            style={{ data: { width: 15, fill: "orange" } }}
+            style={{
+              data: {
+                fill: VictoryTheme.clean.palette?.colors?.orange,
+              },
+            }}
             data={[
               { x: 1, y: 1 },
               { x: 2, y: 2 },
