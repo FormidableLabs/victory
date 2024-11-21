1a8788b1bb63b540fa72b50dfad5e383a17606e6
Stef <ramenhog@gmail.com>
2024-11-21 11:45:14 -0500

    feat: add global overrides to theme builder (#2985)

--- demo/ts/components/theme-builder/utils.ts
+++ demo/ts/components/theme-builder/utils.ts
@@ -2,30 +2,35 @@ import { VictoryThemeDefinition } from "victory-core";
 
 export const setNestedConfigValue = (
   config: VictoryThemeDefinition,
-  path: string,
+  paths: string | string[],
   value: unknown,
 ) => {
-  const pathArray = path.split(".");
   const updatedConfig = { ...config };
-  pathArray.reduce((acc, key, i) => {
-    if (i === pathArray.length - 1) {
-      acc[key] = value;
-    } else {
-      acc[key] = { ...acc[key] };
-    }
-    return acc[key];
-  }, updatedConfig);
+  const pathsArray = Array.isArray(paths) ? paths : [paths];
+
+  pathsArray.forEach((path) => {
+    const pathArray = path.split(".");
+    pathArray.reduce((acc, key, i) => {
+      if (i === pathArray.length - 1) {
+        acc[key] = value;
+      } else {
+        acc[key] = { ...acc[key] };
+      }
+      return acc[key];
+    }, updatedConfig);
+  });
 
   return updatedConfig;
 };
 
 export const getConfigValue = (
   config: VictoryThemeDefinition,
-  path: string,
+  path: string | string[],
   defaultValue?: unknown,
 ) => {
-  if (!path) return undefined;
-  const pathArray = path.split(".");
+  const pathString = Array.isArray(path) ? path[0] : path;
+  if (!pathString) return undefined;
+  const pathArray = pathString.split(".");
   return pathArray.reduce((acc, key) => {
     return acc && acc[key] ? acc[key] : defaultValue || undefined;
   }, config);
