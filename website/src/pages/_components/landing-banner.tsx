989018600d1d99387d0e713f5ced83cb3d469ca5
Nathan Kluth <nlkluth@gmail.com>
2024-12-04 13:20:54 -0700

    tweaks

--- website/src/pages/_components/landing-banner.tsx
+++ website/src/pages/_components/landing-banner.tsx
@@ -2,10 +2,10 @@ import React from "react";
 import { FaArrowRight } from "react-icons/fa";
 
 export const LandingBanner = () => (
-  <div className="bg-[#4589FF] text-white py-2 px-4">
-    <div className="lg:max-w-[90%] flex flex-col sm:flex-row justify-between items-center m-auto">
-      <div className="flex flex-col sm:flex-row items-center gap-2.5">
-        <p className="m-0 text-sm font-bold text-center sm:text-left">
+  <div className="bg-[#4589FF] text-white py-7 px-5 sm:py-2 sm:px-4">
+    <div className="lg:max-w-[90%] flex flex-col sm:flex-row justify-between sm:items-center m-auto">
+      <div className="flex flex-col sm:flex-row gap-2.5">
+        <p className="m-0 text-sm font-bold">
           Like this project? You&apos;ll love working with us.
         </p>
         <p className="m-0 text-sm">
@@ -16,7 +16,7 @@ export const LandingBanner = () => (
         </p>
       </div>
       <a
-        className="text-white text-sm font-bold flex items-center gap-1.5 underline underline-offset-4 decoration-2"
+        className="text-white text-sm font-bold flex items-center justify-end gap-1.5 underline underline-offset-4 decoration-2 hover:text-white mt-2.5 sm:mt-0"
         href="/"
       >
         Learn More
--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -6,14 +6,12 @@ import { LandingFeaturedProjects } from "./_components/landing-featured-projects
 import { LandingFeatures } from "./_components/landing-features";
 import { LandingDemo } from "./_components/landing-demo";
 import { LandingShowcase } from "./_components/landing-showcase";
-import { LandingBanner } from "./_components/landing-banner";
 import CalloutBanner from "../components/CalloutBanner";
 
 // eslint-disable-next-line no-undef
 export default function Home(): JSX.Element {
   return (
     <Layout>
-      <LandingBanner />
       <LandingHero />
       <LandingDemo />
       <LandingFeatures />
