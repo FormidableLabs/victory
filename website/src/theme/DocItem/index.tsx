f863581834e27eaec617d2f1c25db655e41ad705
Nathan Kluth <nlkluth@gmail.com>
2024-12-04 13:28:20 -0700

    mobile callout

---
+++ website/src/theme/DocItem/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import OriginalDocItem from "@theme-original/DocItem";
+import CalloutBanner from "@site/src/components/CalloutBanner";
+
+export default function DocItem(props) {
+  return (
+    <>
+      <OriginalDocItem {...props} />
+      <div className="mt-20 min-[997px]:hidden">
+        <CalloutBanner />
+      </div>
+    </>
+  );
+}
